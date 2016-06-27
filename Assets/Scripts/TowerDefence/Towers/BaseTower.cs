f62b0761c7b8ad36009fd5ef0a42080ece057ef0
Nolimet <nolimet@stamdata.nl>
2016-06-27 21:48:40 +0200

    Updated Utils Folder Did bits of work on the BaseTower

--- Assets/Scripts/TowerDefence/Towers/BaseTower.cs
+++ Assets/Scripts/TowerDefence/Towers/BaseTower.cs
@@ -6,50 +6,116 @@ namespace TowerDefence
 {
     public class BaseTower : MonoBehaviour
     {
-        int fireRate;
-        int Damage;
-        float range;
+        [SerializeField]
+        protected Transform FirePoint;
 
-        public Target_Priority PriorityMode = Target_Priority.Closest;
+        [SerializeField]
+        protected int fireRate;
+        [SerializeField]
+        protected int Damage;
+        [SerializeField]
+        protected float range;
 
-        RaycastHit2D[] hits;
-        BaseEnemy[] targets;
+        public Target_Priority TargetMode = Target_Priority.Closest;
+
+        protected RaycastHit2D[] hits;
+        protected BaseEnemy[] Enemies;
+
         int length;
-        // Update is called once per frame
-        void Update()
+
+        protected Transform Target;
+
+        protected virtual void Update()
         {
             length = Physics2D.CircleCastNonAlloc(transform.position, range, Vector2.zero, hits);
             if (length > 0)
             {
-                targets = new BaseEnemy[length];
+                FillTargetsList();
+                switch (TargetMode)
+                {
+                    case Target_Priority.Closest:
+                        FindClosestTarget();
+                        break;
+
+                    case Target_Priority.First:
+                        GetFirstEntered();
+                        break;
+
+                    case Target_Priority.Last:
+                        break;
+
+                    case Target_Priority.Strongest:
+                        FindStrongest();
+                        break;
+
+                    case Target_Priority.Weakest:
+                        FindWeakest();
+                        break;                       
+                }
+
+                RotateToTarget();
+                FireWeapon();
+            }
+        }
+
+        #region targeting
+        protected virtual void FillTargetsList()
+        {
+            if (TargetMode == Target_Priority.Strongest || TargetMode == Target_Priority.Weakest)
+            {
+                Enemies = new BaseEnemy[length];
                 for (int i = 0; i < length; i++)
                 {
                     if (hits[i].transform.tag == TagManager.Enemy)
                     {
-                        targets[i] = hits[i].transform.gameObject.GetComponent<BaseEnemy>();
+                        Enemies[i] = hits[i].transform.gameObject.GetComponent<BaseEnemy>();
                     }
                 }
             }
         }
 
-        public void FindClosestTarget()
+        protected virtual void FindClosestTarget()
         {
-
+            RaycastHit2D closest = hits[0];
+            foreach(RaycastHit2D hit in hits)
+            {
+                if(hit.distance<closest.distance)
+                {
+                    closest = hit;
+                }
+            }
+            Target = closest.transform;
         }
 
-        public void GetFirstEntered()
+        protected virtual void GetFirstEntered()
         {
-
+            //TODO write code for GetFirst entered
         }
 
-        public void GetStrongest()
+        protected virtual void GetLastEntered()
         {
-
+            //TODO write code for GetLastEntered
         }
 
-        public void GetWeakest()
+        protected virtual void FindStrongest()
         {
+            //TODO write code for FindStrongest
+        }
 
+        protected virtual void FindWeakest()
+        {
+            //TODO write code for FindWeakest
+        }
+        #endregion
+
+        protected virtual void FireWeapon()
+        {
+            //TODO write code for FireWeapon
+        }
+
+        protected virtual void RotateToTarget()
+        {
+            
         }
     }
 }
---
+++ Assets/Scripts/Util/Saving/Serialization.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Util
+{
+    /// <summary>
+    /// class that handels serialization and writing to disk
+    /// </summary>
+    public static class Serialization
+    {
+        [DllImport("__Internal")]
+        private static extern void SyncFiles();
+
+        [DllImport("__Internal")]
+        private static extern void WindowAlert(string message);
+
+        #region fileSaveSettings
+        /// <summary>
+        /// File types that are defined.
+        /// </summary>
+        public enum fileTypes
+        {
+            binary,
+            text
+        }
+
+        /// <summary>
+        /// Location of the save data
+        /// </summary>
+        public static string saveFolderName = "GameData";
+        /// <summary>
+        /// A dictonary contain information related to a filetype
+        /// </summary>
+        readonly public static Dictionary<fileTypes, string> fileExstentions = new Dictionary<fileTypes, string>
+        {
+            { fileTypes.binary,     ".bin"      },
+            { fileTypes.text,       ".txt"     }
+        },
+
+        FileLocations = new Dictionary<fileTypes, string>
+        {
+            { fileTypes.binary,     "Data"      },
+            { fileTypes.text,       "Data"      }
+        };
+        #endregion
+
+        /// <summary>
+        /// Generates a string for where the file is located
+        /// </summary>
+        /// <param name="fileType">The type of file can matter for directory</param>
+        /// <returns></returns>
+        private static string SaveLocation(fileTypes fileType)
+        {
+            
+            string saveLocation = Application.dataPath;
+            if (!Application.isEditor)
+                saveLocation += "/..";
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+                saveLocation = Application.persistentDataPath;
+
+            saveLocation += "/" + saveFolderName + "/" + FileLocations[fileType] + "/";
+            if (!Directory.Exists(saveLocation))
+            {
+                Directory.CreateDirectory(saveLocation);
+            }
+            return saveLocation;
+        }
+
+        /// <summary>
+        /// Returns file type with name attached
+        /// </summary>
+        /// <param name="fileName">The name of the file</param>
+        /// <param name="fileType">The type of file</param>
+        /// <returns>Name + Type </returns>
+        private static string GetFileType(string fileName, fileTypes fileType)
+        {
+            return fileName + fileExstentions[fileType];
+        }
+        
+        /// <summary>
+        /// Save file to disk
+        /// </summary>
+        /// <typeparam name="T">Type of the file</typeparam>
+        /// <param name="fileName">File name with out exstentions</param>
+        /// <param name="fileType">The type of file</param>
+        /// <param name="data">The actual data fo the file</param>
+        public static void Save<T>(string fileName, fileTypes fileType, T data)
+        {
+            string saveFile = SaveLocation(fileType);
+            saveFile += GetFileType(fileName, fileType);
+
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream(saveFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                formatter.Serialize(stream, data);
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+
+                PlatformSafeMessage("Failed to Save: " + e.Message);
+            }
+
+
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+                SyncFiles();
+
+            Debug.Log(System.DateTime.Now + " Saved file: " + saveFile);
+
+        }
+
+        /// <summary>
+        /// Loads a file from disk
+        /// </summary>
+        /// <typeparam name="T">Type of the file</typeparam>
+        /// <param name="fileName"> Name of the file</param>
+        /// <param name="fileType">The file exstention Type</param>
+        /// <param name="outputData">A ref for the file that will be loaded</param>
+        /// <returns>if the loading was succesfull. Needed because a save file can be non existant</returns>
+        public static bool Load<T>(string fileName, fileTypes fileType, ref T outputData)
+        {
+            string saveFile = SaveLocation(fileType);
+            saveFile += GetFileType(fileName, fileType);
+            bool returnval = false;
+
+
+            if (!File.Exists(saveFile))
+            {
+                outputData = default(T);
+                returnval = false;
+            }
+            else
+            {
+                IFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(saveFile, FileMode.Open);
+
+                T data = (T)formatter.Deserialize(stream);
+                outputData = data;
+                returnval = true;
+                stream.Close();
+            }
+            return returnval;
+        }
+
+        /// <summary>
+        /// Used to generate an error when there is one while saving or loading
+        /// </summary>
+        /// <param name="message">The message that will be shown</param>
+        private static void PlatformSafeMessage(string message)
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                WindowAlert(message);
+            }
+            else
+            {
+                Debug.Log(message);
+            }
+        }
+
+    }
+}
