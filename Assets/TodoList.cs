0f2b636f99fe3be4d19d1396b5fb4c0a19447e79
Nolimet <nolimet@stamdata.nl>
2016-12-13 00:15:45 +0100

    added some more stuff like things about playfield size

--- Assets/TodoList.cs
+++ Assets/TodoList.cs
@@ -1,22 +1,25 @@
 ﻿/*
- * TODO:
+ * TODO: WaveManager
  * Finish new wave manager. Rewrite from scratch... Cause it's a mess now
- * Specifications
+ * 
+ * SPECS:
  * - Must be able to hande more than one spawn point
  * - A spawn point a script containing delays and spawn amounts. When script is done it removes it self and says to wave spawner i'm done;
  * - Must keep track of live Enemies
  * - Must send event when no live Enemies remain
- * - 
- * TODO:
- * Add support for more than one node to PathManager.
+ * ----------------------------------------------------------------------------------------
+ * TODO: PathManager
+ * Add support for more than route to PathManager.
  * 
  * CHANGE:
  * Change who nodes are connection. Instead of connecting them in order spawned connect them according to a array system.
+ * 
  * BUILDUP:
  * - Node Positions
  * - Node Type : Spawner, Endpoint, Regular
  * - Node Connections
- * WORKORDER
+ * 
+ * WORKORDER:
  * - SpawnNodes and SetTypes
  * - Connect the nodes according the connection Array
  * 
@@ -24,5 +27,18 @@
  * - Node A : int (node's location in the position array)
  * - Node B : int
  * A connects to B. Objects will move from A to B in a one way trafic
+ * -----------------------------------------------------------------------------------------
+ * TODO: PlayField
+ * Should the size of the playfield be a fixed size or should game have ablity to zoom in and out?
+ * if none fixed size
+ * Write way to move camera about and keep it with in a bounded box.
+ * add background to show field size. Make game default background color a dark color so it's clear that a empty void.
  * 
+ * -----------------------------------------------------------------------------------------
+ * TODO: PLACEMENT MANAGER
+ * add bounding box to playfield so you can't place tower outside the playfield
+ * 
+ * -----------------------------------------------------------------------------------------
+ * TODO: CONTEXTMENU
+ * add right fliped menu so menu is not out of view when near edge of viewfield...
  */
