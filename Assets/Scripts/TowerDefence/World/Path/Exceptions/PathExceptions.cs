d08a390052ad2e290b648045d3ce951883cd5cc0
jesse stam <nolimet@stamdata.nl>
2021-01-08 00:20:32 +0100

    Added a set of exceptions Used to test the paths

---
+++ Assets/Scripts/TowerDefence/World/Path/Exceptions/PathExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TowerDefence.World.Path
+{
+    public class InfitePathException : Exception
+    {
+        public InfitePathException(string message) : base(message)
+        {
+        }
+    }
+
+    public class InvalidPathException : Exception
+    {
+        public InvalidPathException(string message) : base(message)
+        {
+        }
+    }
+
+    public class NoPathEntranceException : Exception
+    {
+        public NoPathEntranceException(string message) : base(message)
+        {
+        }
+    }
+
+    public class NoPathExitException : Exception
+    {
+        public NoPathExitException(string message) : base(message)
+        {
+        }
+    }
+}
