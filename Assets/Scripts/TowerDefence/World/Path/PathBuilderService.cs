ea4851dfaa3619b7c0c56416dbb0938c90d4a0b4
jesse stam <nolimet@stamdata.nl>
2021-01-08 00:20:17 +0100

    added first iteration of Pathbuilder partial validator

--- Assets/Scripts/TowerDefence/World/Path/PathBuilderService.cs
+++ Assets/Scripts/TowerDefence/World/Path/PathBuilderService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TowerDefence.World.Path.Data;
 using UnityEngine;
 
@@ -9,6 +11,51 @@ namespace TowerDefence.World.Path
         public void GenerateWorldPath(PathData pathData)
         {
             throw new NotImplementedException("Generating world path");
+        }
+
+        public bool ValidatePath(PathData pathData)
+        {
+            //TODO add no exit and entrance exception throwing
+            var entrances = pathData.pathPoints.Where(x => x.pointType == PointType.Entrance);
+            var pathPoints = pathData.pathPoints.ToDictionary(point => point.pointId);
+            List<Guid> visitedPoints = new List<Guid>();
+            List<bool> walkResults = new List<bool>();
+
+            foreach (var entrance in entrances)
+            {
+                walkResults.Add(WalkPath(entrance));
+            }
+
+            if (walkResults.Count == 0)
+            {
+            }
+            return walkResults.All(x => x) && walkResults.Count > 0;
+            bool WalkPath(PathPoint current)
+            {
+                if (current.pointType == PointType.Exit)
+                {
+                    return true;
+                }
+
+                if (visitedPoints.Contains(current.pointId))
+                {
+                    List<(bool hasExit, PathPoint point)> connections = new List<(bool hasExit, PathPoint point)>();
+                    visitedPoints.Add(current.pointId);
+
+                    foreach (var pointId in current.pointConnections)
+                    {
+                        if (pathPoints.TryGetValue(pointId, out var point))
+                        {
+                            connections.Add((WalkPath(point), point));
+                        }
+                    }
+
+                    visitedPoints.Remove(current.pointId);
+                    return connections.All(x => x.hasExit) && connections.Count > 0;
+                }
+
+                throw new InfitePathException($"Path has no end! {current}");
+            }
         }
     }
 }
