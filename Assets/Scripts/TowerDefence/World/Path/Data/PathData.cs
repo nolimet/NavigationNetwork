2482449952a727ac869eb400e3a44f0f625b3fd7
jesse stam <nolimet@stamdata.nl>
2021-01-07 23:36:24 +0100

    added Path data and builder service

---
+++ Assets/Scripts/TowerDefence/World/Path/Data/PathData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.World.Path.Data
+{
+    [Serializable]
+    public class PathData
+    {
+        public readonly PathPoint[] pathPoints;
+
+        public PathData(PathPoint[] pathPoints)
+        {
+            this.pathPoints = pathPoints;
+        }
+    }
+
+    [Serializable]
+    public class PathPoint
+    {
+        public readonly Guid pointID;
+        public readonly Vector3 position;
+        public readonly PointType pointType;
+
+        public readonly Guid[] pointConnections;
+
+        public PathPoint(Guid pointID, Vector3 position, PointType pointType, Guid[] pointConnections)
+        {
+            this.pointID = pointID;
+            this.position = position;
+            this.pointType = pointType;
+            this.pointConnections = pointConnections;
+        }
+    }
+
+    [Serializable]
+    public enum PointType
+    {
+        Entrance = 0,
+        Point = 1,
+        Exit = 2
+    }
+}
---
+++ Assets/Scripts/TowerDefence/World/Path/PathBuilderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TowerDefence.World.Path.Data;
+using UnityEngine;
+
+namespace TowerDefence.World.Path
+{
+    public class PathBuilderService
+    {
+        public void GenerateWorldPath(PathData pathData)
+        {
+            throw new NotImplementedException("Generating world path");
+        }
+    }
+}
