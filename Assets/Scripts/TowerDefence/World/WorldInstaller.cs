27b0f9badc21403894404318a8c88e9df1d6919c
jesse stam <nolimet@stamdata.nl>
2021-01-18 22:32:24 +0100

    added path parts to installer

--- Assets/Scripts/TowerDefence/World/WorldInstaller.cs
+++ Assets/Scripts/TowerDefence/World/WorldInstaller.cs
@@ -1,8 +1,21 @@
-﻿using Zenject;
+﻿using TowerDefence.World.Path;
+using UnityEngine;
+using Zenject;
 
 namespace TowerDefence.World
 {
-    public class WorldInstaller : MonoInstaller
+    [CreateAssetMenu(fileName = "World Installer", menuName = "Installers/World Installer")]
+    public class WorldInstaller : ScriptableObjectInstaller
     {
+        [SerializeField]
+        private PathLineRenderer lineGenericPrefab;
+
+        public override void InstallBindings()
+        {
+            Container.BindFactory<PathLineRenderer, PathLineRenderer.Factory>().FromComponentInNewPrefab(lineGenericPrefab);
+
+            Container.Bind<PathBuilderService>().AsSingle();
+            Container.Bind<WorldController>().AsSingle();
+        }
     }
 }
