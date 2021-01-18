fd496efc64b94390d916d25a7ba4ba7637d2d3fb
jesse stam <nolimet@stamdata.nl>
2021-01-18 22:33:42 +0100

    added initial functions for worldController

--- Assets/Scripts/TowerDefence/World/WorldController.cs
+++ Assets/Scripts/TowerDefence/World/WorldController.cs
@@ -1,10 +1,17 @@
 using System;
+using TowerDefence.World.Path;
 using TowerDefence.World.Path.Data;
 
 namespace TowerDefence.World
 {
     public class WorldController
     {
+        private readonly PathBuilderService pathBuilderService;
+
+        public WorldController(PathBuilderService pathBuilderService)
+        {
+            this.pathBuilderService = pathBuilderService;
+        }
         public void SetPath(PathData pathData)
         {
             throw new NotImplementedException("Generate Path!");
