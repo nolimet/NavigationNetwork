904b883232ef99affa10e10b3a1d25962c4c297d
jesse stam <nolimet@stamdata.nl>
2021-01-18 22:34:36 +0100

    Added Pathline render to act as a factory wrapper for lines

---
+++ Assets/Scripts/TowerDefence/World/Path/PathLineRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.World.Path
+{
+    public class PathLineRenderer : MonoBehaviour
+    {
+        [SerializeField]
+        private LineRenderer lineRenderer;
+
+        public void SetPath(Vector3[] path)
+        {
+            lineRenderer.SetPositions(path);
+        }
+
+        public class Factory : PlaceholderFactory<PathLineRenderer>
+        {
+            [Inject]
+            private WorldContainer worldContainer;
+
+            public override PathLineRenderer Create()
+            {
+                var newLine = base.Create();
+                newLine.transform.SetParent(worldContainer.PathContainer);
+                return newLine;
+            }
+
+            public PathLineRenderer Create(Vector3[] path)
+            {
+                var newLine = Create();
+                newLine.SetPath(path);
+                return newLine;
+            }
+        }
+    }
+}
