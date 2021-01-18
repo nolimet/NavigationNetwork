6177aef48e27c0dd4885211022beb048d46a4b54
jesse stam <nolimet@stamdata.nl>
2021-01-18 23:27:12 +0100

    changed so it can be done in a installer

--- Assets/Scripts/TowerDefence/World/WorldContainer.cs
+++ Assets/Scripts/TowerDefence/World/WorldContainer.cs
@@ -12,7 +12,7 @@ namespace TowerDefence.World
         public Transform PathContainer { get; private set; }
         public Transform TurretContainer { get; private set; }
 
-        private void Awake()
+        public void DoSetup()
         {
             PathContainer = new GameObject("Path Container").transform;
             TurretContainer = new GameObject("Turret Container").transform;
