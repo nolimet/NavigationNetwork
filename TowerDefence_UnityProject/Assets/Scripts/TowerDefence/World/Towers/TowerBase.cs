5d6868ef33db7632ed084d9f0636d0170801dc03
jesse stam <nolimet@stamdata.nl>
2021-04-09 22:37:08 +0200

    Added abstract tower managment

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/TowerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.World.Towers
+{
+    public abstract class TowerBase : MonoBehaviour
+    {
+        public abstract void Tick();
+
+        public virtual void Destroy()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/TowerService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.World.Towers
+{
+    public class TowerService : ITickable
+    {
+        private readonly List<TowerBase> towers = new List<TowerBase>();
+        private readonly IInstantiator instantiator;
+
+        public TowerService(IInstantiator instantiator)
+        {
+            this.instantiator = instantiator;
+        }
+
+        public T PlaceTower<T>(T towerPrefab) where T : TowerBase
+        {
+            var newTower = instantiator.InstantiatePrefabForComponent<T>(towerPrefab);
+            towers.Add(newTower);
+
+            return newTower;
+        }
+
+        public void DestroyTower<T>(T tower) where T : TowerBase
+        {
+            if (towers.Contains(tower))
+            {
+                tower.Destroy();
+            }
+        }
+
+        public void Tick()
+        {
+            for (int i = towers.Count - 1; i >= 0; i--)
+            {
+                var tower = towers[i];
+                if (!tower)
+                {
+                    towers.Remove(tower);
+                    continue;
+                }
+
+                tower.Tick();
+            }
+        }
+    }
+}
