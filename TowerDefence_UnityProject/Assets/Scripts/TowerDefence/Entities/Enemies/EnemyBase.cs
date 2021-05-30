416739c879fc84f2c0c3b3c46ab9b81ca70e4fcf
Jesse Stam <nolimet@stamdata.nl>
2021-05-30 21:27:40 +0200

    Added first part of the base enemy

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/Enemies/EnemyBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.World.Path;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Enemies
+{
+    public abstract class EnemyBase : WalkerBase
+    {
+        private Action ReachedEndAction;
+
+        [SerializeField] private double currentHealth = 0;
+        [SerializeField] private double maxHealth = 0;
+
+        private void Awake()
+        {
+            currentHealth = maxHealth;
+        }
+
+        public override void ReachedEnd()
+        {
+            ReachedEndAction?.Invoke();
+        }
+
+        public bool IsDead
+        {
+            get => currentHealth <= 0;
+        }
+    }
+}
