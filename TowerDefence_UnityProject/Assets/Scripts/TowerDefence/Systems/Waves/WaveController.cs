a1e47e39c1f6fc7c0eacd11cf13e040fdfe3fab7
Jesse Stam <nolimet@stamdata.nl>
2021-07-25 10:25:48 +0200

    Implemented a basic version of the wave controller
    It can't yet play a wave but emulate it

--- TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Systems/Waves/WaveController.cs
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Systems/Waves/WaveController.cs
@@ -1,17 +1,74 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TowerDefence.Systems.Waves.Data;
 using UnityEngine;
 
 namespace TowerDefence.Systems.Waves
 {
     public class WaveController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        private void Start()
+        private Wave[] currentWaves;
+        private int activeWave = 0;
+        private CancellationTokenSource cancelTokenSource;
+        private List<Task> activeWaves = new List<Task>();
+
+        public int GetWavesLeft()
         {
+            return activeWave - currentWaves.Length;
         }
 
-        // Update is called once per frame
-        private void Update()
+        public void SetWaves(Wave[] waves)
         {
+            currentWaves = waves;
+
+            if (!cancelTokenSource.IsCancellationRequested)
+                cancelTokenSource.Cancel();
+            cancelTokenSource.Dispose();
+            cancelTokenSource = new CancellationTokenSource();
+            activeWave = 0;
+        }
+
+        public async void StartWavePlayBack()
+        {
+            if (currentWaves != null && currentWaves.Length > 0)
+            {
+                while (activeWave < currentWaves.Length)
+                {
+                    activeWaves.Add(PlayWave(currentWaves[activeWave]));
+                    activeWave++;
+                    try
+                    {
+                        await activeWaves.WaitForAll();
+                    }
+                    catch (System.Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
+                }
+            }
+        }
+
+        public void ForceStartNextWave()
+        {
+            if (currentWaves != null && currentWaves.Length > 0 && GetWavesLeft() > 0)
+            {
+                activeWaves.Add(PlayWave(currentWaves[activeWave]));
+                activeWave++;
+            }
+        }
+
+        public void StopWavePlayBack()
+        {
+            cancelTokenSource.Cancel();
+        }
+
+        private async Task PlayWave(Wave wave)
+        {
+            //process wavedata;
+            Debug.Log("wave started");
+            await new WaitForSeconds(1f);
+            Debug.Log("wave ended");
         }
     }
 }
