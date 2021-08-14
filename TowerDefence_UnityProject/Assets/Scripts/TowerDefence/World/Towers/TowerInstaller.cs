65b96f49a2554247a4600ef28c6ce36fa61f06f2
Jesse Stam <nolimet@stamdata.nl>
2021-08-14 18:18:48 +0200

    added tower installer

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/TowerInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.World.Towers
+{
+    [CreateAssetMenu(fileName = "Tower Installer", menuName = "Installers/Tower Installer")]
+    public class TowerInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField]
+        private TowerConfigurationData towerConfigurationData;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(towerConfigurationData).AsSingle();
+            Container.Bind<TowerService>().ToSelf().AsSingle();
+        }
+    }
+}
