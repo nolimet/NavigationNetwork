329f8b40a82bcf240ef6c825366b3315bdb3be75
Jesse Stam <nolimet@stamdata.nl>
2021-08-14 18:18:10 +0200

    added example tower

---
+++ TowerDefence_UnityProject/Assets/Examples/Towers/ExampleTower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Enemies;
+using TowerDefence.World.Path;
+using TowerDefence.World.Towers;
+using UnityEngine;
+
+namespace Examples.Towers
+{
+    public class ExampleTower : TowerBase
+    {
+        public override float TargetRadius => 10f;
+
+        public override void Tick()
+        {
+            Debug.Log("TICK");
+            var enemies = GetWalkersInRange<EnemyBase>();
+            foreach (var enemy in enemies)
+            {
+                enemy.ApplyDamage(1);
+            }
+        }
+    }
+}
