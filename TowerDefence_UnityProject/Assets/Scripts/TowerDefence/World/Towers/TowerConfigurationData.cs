217374d13ba53799daaac1700fa50f5e45595fa3
Jesse Stam <nolimet@stamdata.nl>
2021-08-14 18:18:33 +0200

    added tower configuration files

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/TowerConfigurationData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TowerDefence.World.Towers
+{
+    [CreateAssetMenu(fileName = "Tower Configuration Data", menuName = "Configuration/Tower Configuration")]
+    public class TowerConfigurationData : ScriptableObject
+    {
+        [SerializeField]
+        private Tower[] towers;
+
+        /// <summary>
+        /// Id's are in lower case to remove any case sensativity
+        /// </summary>
+        public IReadOnlyDictionary<string, AssetReference> Towers { get; private set; }
+
+        public AssetReference GetTower(string id)
+        {
+            if (Towers.TryGetValue(id.ToLower(), out var value))
+            {
+                return value;
+            }
+            return null;
+        }
+
+        private void OnEnable()
+        {
+            if (towers != null && towers.Length > 0)
+            {
+                Towers = towers.ToDictionary(x => x.Id.ToLower(), x => x.Reference);
+            }
+            else
+            {
+                Towers = new Dictionary<string, AssetReference>();
+            }
+        }
+
+        [Serializable]
+        public class Tower
+        {
+            [SerializeField]
+            private string id;
+
+            [SerializeField]
+            private AssetReference reference;
+
+            public AssetReference Reference => reference;
+            public string Id => id;
+        }
+    }
+}
