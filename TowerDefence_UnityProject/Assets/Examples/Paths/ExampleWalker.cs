c0d4bd9c6e36a6e4e6ce05d982295f8ca255d5a6
Jesse Stam <nolimet@stamdata.nl>
2021-09-02 00:27:12 +0200

    Update example walker so it can have a random color

--- TowerDefence_UnityProject/Assets/Examples/Paths/ExampleWalker.cs
+++ TowerDefence_UnityProject/Assets/Examples/Paths/ExampleWalker.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TowerDefence.World.Path;
 using UnityEngine;
+using NoUtil.Extentsions;
+using TowerDefence.Entities.Enemies;
 
-public class ExampleWalker : WalkerBase
+public class ExampleWalker : EnemyBase
 {
+    [SerializeField]
+    private SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        renderer.color = renderer.color.GetRandom();
+        name = "enemy-" + Random.Range(0, 10000);
+    }
+
     public override void ReachedEnd()
     {
-        Debug.Log("Reached End");
     }
 }
