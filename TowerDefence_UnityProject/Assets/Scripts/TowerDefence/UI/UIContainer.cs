b8660982f79e7c3ec2f21f131b149786497d8daa
Jesse Stam <nolimet@stamdata.nl>
2021-12-17 22:40:35 +0100

    Added UI Container to well contain the UI

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/UIContainer.cs
@@ -0,0 +1,29 @@
+﻿using NoUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.UI
+{
+    public class UIContainer : MonoBehaviour
+    {
+        [SerializeField, ReadOnly]
+        private Transform worldUIContainer;
+
+        public Transform WorldUIContainer => worldUIContainer;
+
+        [SerializeField, ReadOnly]
+        private Transform screenUIContainer;
+
+        public Transform ScreenUIContainer => screenUIContainer;
+
+        public void Setup(Transform worldUIContainer, Transform screenUIContainer)
+        {
+            this.worldUIContainer = worldUIContainer;
+            this.screenUIContainer = screenUIContainer;
+        }
+    }
+}
