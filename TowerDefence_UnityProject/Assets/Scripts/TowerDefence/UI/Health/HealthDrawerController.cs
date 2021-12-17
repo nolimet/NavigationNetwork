84fb2281a06e602b05675c6e53fdb0e16af0021e
Jesse Stam <nolimet@stamdata.nl>
2021-12-17 22:40:15 +0100

    Added a controller for drawing the health bars

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/Health/HealthDrawerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Enemies;
+using Zenject;
+
+namespace TowerDefence.UI.Health
+{
+    public class HealthDrawerController : ITickable
+    {
+        private readonly HealthDrawer.Factory healthDrawerFactory;
+        private readonly List<HealthDrawer> healthDrawers = new();
+
+        public HealthDrawerController(HealthDrawer.Factory healthDrawerFactory)
+        {
+            this.healthDrawerFactory = healthDrawerFactory;
+        }
+
+        public void AddHealthBar(EnemyBase enemyBase)
+        {
+            healthDrawers.Add(healthDrawerFactory.Create(enemyBase));
+        }
+
+        public void Tick()
+        {
+            TrimDrawers();
+            foreach (HealthDrawer healthDrawer in healthDrawers)
+            {
+                healthDrawer.UpdateHealthBar();
+            }
+        }
+
+        private void TrimDrawers()
+        {
+            if (healthDrawers.Any(x => !x.TargetIsAlive()))
+            {
+                var deadDrawers = healthDrawers.Where(x => !x.TargetIsAlive()).ToArray();
+                foreach (var deadDrawer in deadDrawers)
+                {
+                    deadDrawer.Destroy();
+                    healthDrawers.Remove(deadDrawer);
+                }
+            }
+        }
+    }
+}
