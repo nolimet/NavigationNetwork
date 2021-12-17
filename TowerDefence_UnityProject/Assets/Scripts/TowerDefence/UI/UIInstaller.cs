3b64a50dd7439e98b01e7f3d17a8706a136f3e04
Jesse Stam <nolimet@stamdata.nl>
2021-12-17 22:44:02 +0100

    Implemented the actual UI Installer code

--- TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/UIInstaller.cs
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/UIInstaller.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TowerDefence.UI.Health;
+using UnityEngine;
+using Zenject;
 
-namespace Assets.Scripts.TowerDefence.UI
+namespace TowerDefence.UI
 {
-    class UIInstaller
+    [CreateAssetMenu(fileName = "UI Installer", menuName = "Installers/UI Installer")]
+    public class UIInstaller : ScriptableObjectInstaller
     {
+        [SerializeField]
+        private HealthDrawer healthbarPrefab;
+
+        [SerializeField]
+        private GameObject worldUIContainerPrefab;
+
+        [SerializeField]
+        private GameObject screenUIContainerPrefab;
+
+        public override void InstallBindings()
+        {
+            var uiContainer = new GameObject("UI Container", typeof(UIContainer)).GetComponent<UIContainer>();
+            var worldUIContainer = Instantiate(worldUIContainerPrefab, uiContainer.transform, false);
+            var screenUIContainer = Instantiate(screenUIContainerPrefab, uiContainer.transform, false);
+
+            uiContainer.Setup(worldUIContainer.transform, screenUIContainer.transform);
+
+            Container.BindInstance(uiContainer).AsSingle();
+            Container.BindInterfacesAndSelfTo<HealthDrawerController>().AsSingle();
+
+            Container.BindFactory<HealthDrawer, HealthDrawer.Factory>().FromComponentInNewPrefab(healthbarPrefab);
+        }
     }
 }
