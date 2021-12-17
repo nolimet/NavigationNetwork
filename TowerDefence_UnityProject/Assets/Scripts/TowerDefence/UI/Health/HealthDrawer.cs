0dd105ea7d0ef76549704ef1537239afe521bd17
Jesse Stam <nolimet@stamdata.nl>
2021-12-17 22:41:01 +0100

    Updated Ui installer to container actual funtions

--- TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/Health/HealthDrawer.cs
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/Health/HealthDrawer.cs
@@ -19,14 +19,33 @@ namespace TowerDefence.UI.Health
 
         public void UpdateHealthBar()
         {
-            healthbarImage.fillAmount = (float)(targetEnemy.CurrentHealth / targetEnemy.MaxHealth);
+            if (targetEnemy)
+            {
+                healthbarImage.fillAmount = (float)(targetEnemy.CurrentHealth / targetEnemy.MaxHealth);
+                transform.position = targetEnemy.transform.position + targetEnemy.HealthBarOffset;
+            }
+        }
+
+        public bool TargetIsAlive()
+        {
+            return targetEnemy;
+        }
+
+        public void Destroy()
+        {
+            Destroy(gameObject);
         }
 
         public class Factory : PlaceholderFactory<HealthDrawer>
         {
+            [Inject]
+            private UIContainer uiContainer;
+
             public HealthDrawer Create(EnemyBase targetEnemy)
             {
                 var newHealthBar = base.Create();
+
+                newHealthBar.transform.SetParent(uiContainer.WorldUIContainer, false);
                 newHealthBar.targetEnemy = targetEnemy;
 
                 return newHealthBar;
