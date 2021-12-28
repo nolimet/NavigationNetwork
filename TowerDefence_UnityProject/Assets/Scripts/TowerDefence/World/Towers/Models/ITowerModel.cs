5165c7e7f27d87c75bd382251b431c101238a1b4
Jesse Stam <nolimet@stamdata.nl>
2021-12-28 15:59:20 +0100

    Created a model for the towers

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/Models/ITowerModel.cs
@@ -0,0 +1,14 @@
+﻿using DataBinding.BaseClasses;
+using DataBinding.Helpers;
+using System.Collections.Generic;
+
+namespace TowerDefence.World.Towers.Models
+{
+    [DataModel(AddToZenject = true, Shared = true)]
+    public interface ITowerModel : IModelBase
+    {
+        IList<TowerBase> Towers { get; set; }
+
+        TowerBase SelectedTower { get; set; }
+    }
+}
