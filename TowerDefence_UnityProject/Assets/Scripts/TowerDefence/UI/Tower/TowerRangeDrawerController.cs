3e789817c370a9f1e1bd10475a20e6eabdfa7354
Jesse Stam <nolimet@stamdata.nl>
2022-01-04 23:28:54 +0100

    Added TowerRangeDrawerController

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/Tower/TowerRangeDrawerController.cs
@@ -0,0 +1,68 @@
+﻿using DataBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Systems.Selection;
+using TowerDefence.Systems.Selection.Models;
+using TowerDefence.World.Towers;
+using TowerDefence.World.Towers.Models;
+using UnityEngine.AddressableAssets;
+
+namespace TowerDefence.UI.Tower
+{
+    public class TowerRangeDrawerController
+    {
+        private readonly ISelectionModel selectionModel;
+        private readonly ITowerModels towerModels;
+        private readonly BindingContext bindingContext = new(true);
+        private TowerRangeDrawer rangeDrawer;
+
+        public TowerRangeDrawerController(ISelectionModel selectionModel, ITowerModels towerModels, AssetReference rangeDrawer)
+        {
+            this.selectionModel = selectionModel;
+            this.towerModels = towerModels;
+
+            bindingContext.Bind(selectionModel, x => x.Selection, SelectionChanged);
+
+            SetRangeDrawer(rangeDrawer);
+        }
+
+        private async void SetRangeDrawer(AssetReference rangeDrawer)
+        {
+            await AsyncAwaiters.NextFrame;
+            this.rangeDrawer = await GetTowerRangeDrawer();
+            async Task<TowerRangeDrawer> GetTowerRangeDrawer()
+            {
+                var task = rangeDrawer.InstantiateAsync();
+                await task;
+                return task.Result.GetComponent<TowerRangeDrawer>();
+            }
+        }
+
+        private void SelectionChanged(IList<ISelectable> selection)
+        {
+            var subSelection = selection.Where(x => x is TowerBase);
+            if (subSelection.Count() == 0)
+            {
+                rangeDrawer?.gameObject.SetActive(false);
+            }
+            else if (subSelection.First() is TowerBase tower)
+            {
+                var towerModel = towerModels.Towers.FirstOrDefault(x => x.TowerRenderer == tower);
+
+                rangeDrawer.gameObject.SetActive(towerModel?.TowerRenderer);
+                if (towerModel?.TowerRenderer)
+                {
+                    rangeDrawer.DrawRange(towerModel);
+                }
+            }
+        }
+
+        ~TowerRangeDrawerController()
+        {
+            bindingContext.Dispose();
+        }
+    }
+}
