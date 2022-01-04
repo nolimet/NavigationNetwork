30b6e7b17d581e6179182c4d6a8eb79c1b8f5441
Jesse Stam <nolimet@stamdata.nl>
2022-01-04 23:27:20 +0100

    Created input installer and selection input

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Input/InputInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.Input
+{
+    [CreateAssetMenu(fileName = "Input Installer", menuName = "Installers/Input Installer")]
+    public class InputInstaller : ScriptableObjectInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<SelectionInputActions>().AsSingle();
+        }
+    }
+}
