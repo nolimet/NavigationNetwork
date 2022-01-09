01d37ccee77fd1a812c3381d45a96ac9d481ef3f
Jesse Stam <nolimet@stamdata.nl>
2022-01-09 23:42:32 +0100

    Added ITowerObject to represent the tower in the background

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/Object/ITowerObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.World.Towers
+{
+    public interface ITowerObject
+    {
+        Vector3 GetWorldPosition();
+        Vector2 GetGridPosition();
+        Transform Transform { get; }
+    }
+}
---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/Object/TowerObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.World.Towers
+{
+    public class TowerObject : MonoBehaviour ,ITowerObject
+    {
+        public Transform Transform => transform;
+
+        public Vector2 GetGridPosition() => throw new NotImplementedException("TODO Implement grid system");
+
+        public Vector3 GetWorldPosition() => transform.position;
+    }
+}
