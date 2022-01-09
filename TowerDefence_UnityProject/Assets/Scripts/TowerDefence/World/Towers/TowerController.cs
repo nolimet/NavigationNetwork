36f278ebb4ed4d4bde16b832fd6065573c47ca30
Jesse Stam <nolimet@stamdata.nl>
2022-01-09 23:42:49 +0100

    Added a tower controller to handle ticking of the towers

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/TowerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.World.Towers.Components;
+using TowerDefence.World.Towers.Models;
+using Zenject;
+
+namespace TowerDefence.World.Towers
+{
+    public class TowerController : ITickable
+    {
+        private readonly ITowerModels towerModels;
+
+        public TowerController(ITowerModels towerModels)
+        {
+            this.towerModels = towerModels;
+        }
+
+        public void Tick()
+        {
+            foreach (var model in this.towerModels.Towers)
+            {
+                foreach (var component in model.Components.Where(x => x is ITickableTowerComponent).Cast<ITickableTowerComponent>().OrderBy(x => x.TickPriority))
+                {
+                    component.Tick();
+                }
+            }
+        }
+    }
+}
