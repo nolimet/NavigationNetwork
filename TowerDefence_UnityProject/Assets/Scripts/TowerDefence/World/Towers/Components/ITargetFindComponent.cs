99b28df63b2a1076dcd7923dfd4bbac8783283f0
Jesse Stam <nolimet@stamdata.nl>
2022-01-09 23:44:51 +0100

    Created TargetFindComponent

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/Components/ITargetFindComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Enemies;
+using TowerDefence.Entities.Enemies.Models;
+
+namespace TowerDefence.World.Towers.Components
+{
+    public interface ITargetFindComponent : ITickableTowerComponent
+    {
+        /// <summary>
+        /// List of defined list of a single or multiple targets to apply any effects too
+        /// </summary>
+        IEnumerable<IEnemyObject> FoundTarget { get; }
+    }
+}
