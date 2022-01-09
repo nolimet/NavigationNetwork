47281505674afcccb58429dde0470f07a9107886
Jesse Stam <nolimet@stamdata.nl>
2022-01-09 23:44:25 +0100

    Added a tickable version of the TowerComponent

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/World/Towers/Components/ITickableTowerComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace TowerDefence.World.Towers.Components
+{
+    public interface ITickableTowerComponent : ITowerComponent
+    {
+        /// <summary>
+        /// The lower the value the higher the priorty use any value between -32,768 to 32,767
+        /// </summary>
+        short TickPriority { get; }
+
+        void Tick();
+    }
+}
