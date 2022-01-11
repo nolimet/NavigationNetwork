59acf9c1b7c38a863591ae58e35fccf64f586076
Jesse Stam <nolimet@stamdata.nl>
2022-01-11 21:26:35 +0100

    Added enitty base object

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/IEntityObjectBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Systems.Selection;
+using UnityEngine;
+
+namespace TowerDefence.Entities
+{
+    public interface IEntityObjectBase : ISelectable
+    {
+        public Transform Transform { get; }
+
+        public Vector3 GetWorldPosition();
+    }
+}
