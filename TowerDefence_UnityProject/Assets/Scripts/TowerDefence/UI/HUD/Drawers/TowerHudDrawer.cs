c828cabd79c54465db8bce6c5a4c00ed2091c388
Jesse Stam <nolimet@stamdata.nl>
2022-01-17 23:21:22 +0100

    Added Tower Hud Drawer (WIP)

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/UI/HUD/Drawers/TowerHudDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using TMPro;
+using TowerDefence.Entities.Towers;
+using TowerDefence.Entities.Towers.Components;
+using TowerDefence.Entities.Towers.Models;
+using UnityEngine;
+
+namespace TowerDefence.UI.HUD
+{
+    public class TowerHudDrawer : MonoBehaviour
+    {
+        [SerializeField] private string rangeLabel, damageLabel, towerTypeLabel;
+        [SerializeField] private BaseHudValueItem rangeField, damageField, towerType;
+
+        public void SetActive(bool isActive) => gameObject.SetActive(isActive);
+
+        public void SetValues(ITowerObject tower)
+        {
+            rangeField.SetValue(tower.Model.Range.ToString());
+
+            var damageComponent = tower.Model.Components.FirstOrDefault(x => x is IDamageComponent) as IDamageComponent;
+            damageField.SetValue((damageComponent?.DamagePerSecond ?? 0d).ToString());
+
+            towerType.SetValue(tower.Name);
+        }
+    }
+}
