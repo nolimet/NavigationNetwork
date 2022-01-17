42e75e426e58fa28637b0befecf195ea233b696a
Jesse Stam <nolimet@stamdata.nl>
2022-01-17 23:19:58 +0100

    Added Tick, Name and Destroy To IEntityObjectBase

--- TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/IEntityObjectBase.cs
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/IEntityObjectBase.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TowerDefence.Systems.Selection;
+﻿using TowerDefence.Systems.Selection;
 using UnityEngine;
 
 namespace TowerDefence.Entities
 {
     public interface IEntityObjectBase : ISelectable
     {
+        public string Name { get; set; }
         public Transform Transform { get; }
 
         public Vector3 GetWorldPosition();
+
+        public void Destroy() => Object.Destroy(Transform.gameObject);
+
+        public void Tick();
     }
 }
