c4b8a04cabcb8e80caa6cb4780ed8daeed0cc1aa
Jesse Stam <nolimet@stamdata.nl>
2022-01-17 23:16:11 +0100

    Added first simple damage component

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/Towers/Components/Damage/DamageComponentBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Towers.Models;
+
+namespace TowerDefence.Entities.Towers.Components.Damage
+{
+    public abstract class DamageComponentBase : IDamageComponent
+    {
+        protected readonly ITowerModel model;
+
+        protected DamageComponentBase(ITowerModel model)
+        {
+            this.model = model;
+        }
+
+        public abstract double DamagePerSecond { get; }
+
+        public abstract void Tick();
+    }
+}
