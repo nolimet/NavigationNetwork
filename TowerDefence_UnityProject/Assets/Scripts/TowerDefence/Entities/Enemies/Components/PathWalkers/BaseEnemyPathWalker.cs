82731e7a5868cceb53dcd3fa8c52704e7c7013d4
Jesse Stam <nolimet@stamdata.nl>
2022-01-25 00:10:46 +0100

    Added base for component system for enemies

---
+++ TowerDefence_UnityProject/Assets/Scripts/TowerDefence/Entities/Enemies/Components/PathWalkers/BaseEnemyPathWalker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Events;
+
+namespace TowerDefence.Entities.Enemies.Components
+{
+    public abstract class BaseEnemyPathWalker : IPathWalkerComponent
+    {
+        protected readonly UnityAction<IEnemyObject> reachedEnd;
+
+        protected BaseEnemyPathWalker(UnityAction<IEnemyObject> reachedEnd)
+        {
+            this.reachedEnd = reachedEnd;
+        }
+
+        public short TickPriority => short.MinValue;
+
+        public abstract float PathProgress { get; protected set; }
+
+        public abstract void Tick();
+    }
+}
