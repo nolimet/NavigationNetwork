733a33b955f2fd9e51a9181c7585c4e2fb83e853
Jesse Stam <nolimet@stamdata.nl>
2022-01-30 23:15:30 +0100

    Added TowerComponent Attribute

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Builder/TowerComponentAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TowerDefence.Entities.Towers.Builder
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = true, AllowMultiple = false)]
+    internal sealed class TowerComponentAttribute : Attribute
+    {
+    }
+}
