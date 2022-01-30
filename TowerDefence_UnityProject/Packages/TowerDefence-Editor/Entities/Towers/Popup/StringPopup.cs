78279c750d9d65bbf87db785a4945e1257e84083
Jesse Stam <nolimet@stamdata.nl>
2022-01-30 23:17:20 +0100

    Added first itteration of TowerObjectEditor and a popup for it

---
+++ TowerDefence_UnityProject/Packages/TowerDefence-Editor/Entities/Towers/Popup/StringPopup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Towers.Popup
+{
+    internal class StringPopup : PopupWindowContent
+    {
+        private readonly IEnumerable<string> values;
+        public string SelectedValue { get; private set; } = string.Empty;
+        public bool InputCancelled { get; private set; }
+
+        private Vector2 scrollRectPosition = Vector2.zero;
+
+        public StringPopup(IEnumerable<string> values)
+        {
+            this.values = values;
+        }
+
+        public override Vector2 GetWindowSize()
+        {
+            return new Vector2(400, 800);
+        }
+
+        public override void OnGUI(Rect rect)
+        {
+            using (var h1 = new EditorGUILayout.HorizontalScope())
+            {
+                using (var d1 = new EditorGUI.DisabledGroupScope(SelectedValue == string.Empty))
+                {
+                    if (GUILayout.Button("Confirm"))
+                    {
+                        InputCancelled = false;
+                        editorWindow.Close();
+                    }
+                }
+
+                if (GUILayout.Button("Cancel"))
+                {
+                    InputCancelled = true;
+                    editorWindow.Close();
+                }
+            }
+            using (var scrollRect = new EditorGUILayout.ScrollViewScope(scrollRectPosition))
+            {
+                foreach (string value in values)
+                {
+                    using (var disableGroup = new EditorGUI.DisabledGroupScope(value == SelectedValue))
+                    {
+                        if (GUILayout.Button(value))
+                        {
+                            SelectedValue = value;
+                        }
+                    }
+                }
+
+                scrollRectPosition = scrollRect.scrollPosition;
+            }
+        }
+    }
+}
---
+++ TowerDefence_UnityProject/Packages/TowerDefence-Editor/Entities/Towers/TowerObjectEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TowerDefence.Entities.Towers.Builder;
+using TowerDefence.Entities.Towers.Builder.Data;
+using TowerDefence.Entities.Towers.Components;
+using TowerDefence.Entities.Towers.Popup;
+using UnityEditor;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Towers
+{
+    [CustomEditor(typeof(TowerConfigurationObject))]
+    internal class TowerObjectEditor : Editor
+    {
+        private readonly List<Type> componentTypes = new();
+        private readonly List<string> componentNames = new();
+
+        private readonly Dictionary<TowerComponentData, ITowerComponent> componentsCache = new();
+
+        public override void OnInspectorGUI()
+        {
+            var target = this.target as TowerConfigurationObject;
+            if (GUILayout.Button("Add Component"))
+            {
+                var popup = new StringPopup(componentNames);
+                PopupWindow.Show(GUILayoutUtility.GetLastRect(), popup);
+
+                Debug.Log(popup.SelectedValue);
+            }
+
+            foreach (var type in componentNames)
+            {
+                EditorGUILayout.LabelField(type);
+            }
+        }
+
+        private void OnEnable()
+        {
+            componentsCache.Clear();
+
+            //Get all the marked components
+            AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(x => x.FullName.ToLower().Contains("towerdefence")))
+            {
+                componentTypes.AddRange(assembly.GetTypes().Where(t => t.IsDefined(typeof(TowerComponentAttribute)) && !t.IsAbstract));
+            }
+
+            foreach (var type in componentTypes)
+            {
+                componentNames.Add(type.ToString().Replace("TowerDefence.Entities.Towers.Components.", ""));
+            }
+        }
+    }
+}
