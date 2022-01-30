a6a5e0a77da65c9d401d55965f73b86ca3d8fc6c
Jesse Stam <nolimet@stamdata.nl>
2022-01-30 23:16:55 +0100

    Added tower configuration Object and some example functions

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Builder/Data/TowerComponentData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using TowerDefence.Entities.Towers.Components;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Towers.Builder.Data
+{
+    [Serializable]
+    internal class TowerComponentData
+    {
+        [SerializeField] private string type;
+        [SerializeField] internal byte[] componentData;
+
+        internal void SerializeTowerComponent(ITowerComponent component)
+        {
+            type = component.GetType().ToString().Replace("TowerDefence.Entities.Towers.Components.", "");
+            using (var memoryStream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memoryStream, component);
+                componentData = memoryStream.ToArray();
+            }
+        }
+
+        internal ITowerComponent DeserializeTowerComponent()
+        {
+            ITowerComponent result = null;
+            try
+            {
+                using (var memoryStream = new MemoryStream(this.componentData))
+                {
+                    var formatter = new BinaryFormatter();
+                    var r = formatter.Deserialize(memoryStream);
+                    Debug.Log(r);
+
+                    result = r as ITowerComponent;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                return default;
+            }
+            return result;
+        }
+    }
+}
---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Builder/Data/TowerConfigurationObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using TowerDefence.Entities.Towers.Components;
+using TowerDefence.Entities.Towers.Components.Damage;
+using TowerDefence.Entities.Towers.Components.TargetFinders;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Towers.Builder.Data
+{
+    [CreateAssetMenu(menuName = "Entities/Towers")]
+    internal class TowerConfigurationObject : ScriptableObject
+    {
+        [SerializeField]
+        internal List<TowerComponentData> components;
+
+        [ContextMenu("Test-Write")]
+        private void TestWrite()
+        {
+            List<ITowerComponent> components = new();
+            if (this.components == null)
+            {
+                this.components = new();
+            }
+
+            components.Add(new NearestTargetFinder());
+            components.Add(new DamageAllTargets(10, 10));
+
+            foreach (var component in components)
+            {
+                var componentData = new TowerComponentData();
+                componentData.SerializeTowerComponent(component);
+                this.components.Add(componentData);
+            }
+        }
+
+        [ContextMenu("Test-Read")]
+        private void TestRead()
+        {
+            List<ITowerComponent> components = new();
+
+            if (this.components != null)
+            {
+                foreach (var component in this.components)
+                {
+                    components.Add(component.DeserializeTowerComponent());
+                }
+
+                foreach (var component in components)
+                {
+                    if (component != null)
+                        Debug.Log(component.GetType());
+                    else
+                        Debug.Log(component);
+                }
+            }
+        }
+    }
+}
