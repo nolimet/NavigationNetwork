30804f2ab65d9764d6788e06cebb56992ccf8145
Jesse Stam <nolimet@stamdata.nl>
2022-01-30 23:13:15 +0100

    added assembly info

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TowerDefence-Editor")]
+
+namespace TowerDefence.Packages.TowerDefence
+{
+    public class AssemblyInfo
+    {
+    }
+}
