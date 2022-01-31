da000efd332d2327eca5fd38895b036db23367ac
Jesse Stam <nolimet@stamdata.nl>
2022-01-31 21:09:02 +0100

    added damage first target class

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/Damage/DamageFirstTarget.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using TowerDefence.Entities.Towers.Builder;
+using TowerDefence.Entities.Towers.Models;
+using UnityEngine;
+
+namespace TowerDefence.Entities.Towers.Components.Damage
+{
+    [Serializable, TowerComponent]
+    internal class DamageFirstTarget : DamageComponentBase
+    {
+        [JsonProperty] private readonly double damage;
+        [JsonProperty] private readonly float damageInterval;
+
+        [NonSerialized] private float intervalTimer = 0f;
+        public override double DamagePerSecond => damage / damageInterval;
+
+        public override void PostInit(ITowerModel model)
+        {
+            base.PostInit(model);
+
+            if (damageInterval < 0f)
+            {
+                throw new ArgumentOutOfRangeException("Invalid attack cooldown Time. Value should be greater or equal then 0");
+            }
+        }
+
+        public override void Tick()
+        {
+            intervalTimer -= Time.deltaTime;
+            if (intervalTimer <= 0 && targetFindComponent != null && targetFindComponent.FoundTargets.Any())
+            {
+                intervalTimer = damageInterval;
+
+                targetFindComponent.FoundTargets.First().Damage(damage);
+            }
+        }
+    }
+}
