0e18929a3e67a5da39d59894ef5158e46c6535d2
Jesse Stam <nolimet@stamdata.nl>
2022-02-04 23:10:55 +0100

    Added a tower settings object

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/BaseComponents/TowerSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Components;
+
+namespace TowerDefence.Entities.Towers.Components.BaseComponents
+{
+    [Component(ComponentType.Tower)]
+    internal class TowerSettings : IComponent
+    {
+        public readonly string Name;
+        public readonly double Range;
+    }
+}
