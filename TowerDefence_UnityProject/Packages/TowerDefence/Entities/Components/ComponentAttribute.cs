a2efec0054e8fb7c6f15633266234561468c9246
Jesse Stam <jesse@jessestam.nl>
2022-02-15 08:37:59 +0100

    Added restrictionSupport to components

--- TowerDefence_UnityProject/Packages/TowerDefence/Entities/Components/ComponentAttribute.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Components/ComponentAttribute.cs
@@ -6,10 +6,31 @@ namespace TowerDefence.Entities.Components
     internal sealed class ComponentAttribute : Attribute
     {
         public readonly ComponentType ComponentType;
+        public readonly Type[] restirctions;
+        public readonly bool AllowMultiple = false;
 
         public ComponentAttribute(ComponentType componentType, params Type[] restirctions)
         {
             this.ComponentType = componentType;
+            this.restirctions = restirctions;
+        }
+
+        internal bool AnyRestrictionsMatch(Type self, Type other)
+        {
+            if (!AllowMultiple && other == self)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < restirctions.Length; i++)
+            {
+                if (restirctions[i] == other && restirctions[i] != self)
+                {
+                    return false;
+                }
+            }
+
+           return false;
         }
     }
 
