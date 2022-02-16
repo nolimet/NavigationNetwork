ab05aa98458be2f4e4239ac026c81d23b307c62c
Jesse Stam <jesse@jessestam.nl>
2022-02-16 08:38:36 +0100

    Added find all targets target finder

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/TargetFinders/AllTargetsFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefence.Entities.Components;
+using TowerDefence.Entities.Towers.Components.Interfaces;
+
+namespace TowerDefence.Entities.Towers.Components.TargetFinders
+{
+    [Serializable, Component(ComponentType.Tower, typeof(ITargetFindComponent))]
+    internal class AllTargetsFinder : TargetFindBase
+    {
+        public override void Tick()
+        {
+            targetList.Clear();
+            targetList.AddRange(GetEnemyObjectsInRange());
+        }
+    }
+}
