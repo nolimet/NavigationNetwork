e8c8251653528c3f33b24dc9f952b7b47064afe4
Jesse Stam <nolimet@stamdata.nl>
2022-02-18 18:14:10 +0100

    Changed serialization of component data

--- TowerDefence_UnityProject/Packages/TowerDefence/Entities/Components/Data/ComponentData.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Components/Data/ComponentData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -10,36 +11,17 @@ namespace TowerDefence.Entities.Components.Data
     internal class ComponentData
     {
         [SerializeField] internal string type;
-        [SerializeField] internal byte[] componentData;
+        [SerializeField] internal string data;
 
         internal void SerializeComponent(IComponent component)
         {
-            type = component.GetType().ToString().Replace("TowerDefence.Entities.", "");
-            using (var memoryStream = new MemoryStream())
-            {
-                IFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(memoryStream, component);
-                componentData = memoryStream.ToArray();
-            }
+            type = component.GetType().ToString();
+            data = JsonConvert.SerializeObject(component);
         }
 
         internal IComponent DeserializeComponent()
         {
-            IComponent result = null;
-            try
-            {
-                using (var memoryStream = new MemoryStream(this.componentData))
-                {
-                    var formatter = new BinaryFormatter();
-                    result = formatter.Deserialize(memoryStream) as IComponent; ;
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogException(ex);
-                return default;
-            }
-            return result;
+            return JsonConvert.DeserializeObject(data, Type.GetType(type)) as IComponent;
         }
     }
 }
