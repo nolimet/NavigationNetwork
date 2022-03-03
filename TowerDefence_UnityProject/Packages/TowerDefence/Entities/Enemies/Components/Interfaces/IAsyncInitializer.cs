78cd9884dada5670712bc7d8ad7ddc0d0f9e893a
Jesse Stam <nolimet@stamdata.nl>
2022-03-03 23:34:54 +0100

    Moved and added intitalizer interfaces for enemies

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Enemies/Components/Interfaces/IAsyncInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TowerDefence.Entities.Enemies.Models;
+
+namespace TowerDefence.Entities.Enemies.Components.Interfaces
+{
+    internal interface IAsyncInitializer
+    {
+        Task AsyncPostInit(IEnemyObject EnemyObject, IEnemyModel enemyModel);
+    }
+}
