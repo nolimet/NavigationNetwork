1d19110d1a848b38e9e8a3da3d436ffd6a5bf504
Jesse Stam <nolimet@stamdata.nl>
2022-03-03 23:35:16 +0100

    Added EnemySettings

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Enemies/Components/BaseComponents/EnemySettings.cs
@@ -0,0 +1,14 @@
+﻿using TowerDefence.Entities.Components;
+
+namespace TowerDefence.Entities.Enemies.Components.BaseComponents
+{
+    [System.Serializable]
+    internal class EnemySettings : IComponent
+    {
+        public readonly string TypeName;
+        public readonly string BaseName;
+
+        public readonly double MaxHealth;
+        public readonly double Speed;
+    }
+}
