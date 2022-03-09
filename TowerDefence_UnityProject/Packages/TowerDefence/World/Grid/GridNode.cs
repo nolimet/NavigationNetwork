a5f63bb3ad312b3fa9daf74929d27fc74d5c3802
Jesse Stam <jesse@jessestam.nl>
2022-03-09 08:43:36 +0100

    Setup for path finding

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence.World.Grid
+{
+    internal class GridNode : IGridNode
+    {
+        public double NodeWeight { get; private set; }
+
+        public bool HasStructure { get; private set; }
+
+        public IReadOnlyCollection<IGridNode> ConnectedNodes { get; private set; }
+
+        public Vector2Int Position { get; private set; }
+
+        public GridNode(double nodeWeight, Vector2Int position)
+        {
+            NodeWeight = nodeWeight;
+            Position = position;
+        }
+
+        public void SetConnectedNodes(IReadOnlyCollection<IGridNode> connectedNodes)
+        {
+            ConnectedNodes = connectedNodes;
+        }
+
+    }
+}
---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/PathFinder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TowerDefence.World.Grid
+{
+    internal class PathFinder
+    {
+        public IReadOnlyList<IGridNode> GetPath(IGridNode start, IGridNode end)
+        {
+
+        }
+    }
+}
