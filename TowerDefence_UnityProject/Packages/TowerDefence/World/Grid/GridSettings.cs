a4bd19e2369aa2627e31e5a708a5f9eef66ee91a
Jesse Stam <jesse@jessestam.nl>
2022-03-16 08:47:51 +0100

    Added a custom converter for bool arrays

--- TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridSettings.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridSettings.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace TowerDefence.World.Grid
@@ -7,16 +8,30 @@ namespace TowerDefence.World.Grid
     [Serializable]
     internal sealed class GridSettings
     {
-        [field: SerializeField]
+        public GridSettings(int gridHeight, int gridWidth, bool[] gridLayout)
+        {
+            GridHeight = gridHeight;
+            GridWidth = gridWidth;
+            this.gridLayout = gridLayout;
+        }
+
         [JsonProperty] public int GridHeight { get; private set; }
 
-        [field: SerializeField]
         [JsonProperty] public int GridWidth { get; private set; }
 
         /// <summary>
         /// Grid weights and layout. -255 is not traversable
         /// </summary>
-        [field: SerializeField]
-        [JsonProperty] public byte[] gridLayout { get; private set; }
+        [JsonProperty] public bool[] gridLayout { get; private set; }
+
+        [MenuItem("Test/Settings")]
+        public static void TestWrite()
+        {
+            var settings = new GridSettings(10, 10, new[] { true, true, true, true, false, false, true, true, false });
+            string json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+            Debug.Log(json);
+            var val = JsonConvert.DeserializeObject<GridSettings>(json);
+            Debug.Log(string.Join(",", val.gridLayout));
+        }
     }
 }
