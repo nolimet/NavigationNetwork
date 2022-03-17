d927b3274e2b726d6262e3ae7fae6b3c998d083e
Jesse Stam <jesse@jessestam.nl>
2022-03-17 08:51:14 +0100

    Added grid generation

--- TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridGenerator.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TowerDefence.World.Grid
 {
@@ -7,7 +8,66 @@ namespace TowerDefence.World.Grid
     {
         public IEnumerable<IGridNode> CreateNodes(GridSettings settings)
         {
-            throw new NotImplementedException();
+            Validate();
+            var nodes = new GridNode[settings.GridHeight][];
+            CreateNodes();
+            LinkNodes();
+
+            return nodes.SelectMany(x => x).Cast<IGridNode>().ToArray();
+
+            void LinkNodes()
+            {
+                List<IGridNode> neightbours = new();
+                for (int y = 0; y < settings.GridHeight; y++)
+                {
+                    for (int x = 0; x < settings.GridWidth; x++)
+                    {
+                        neightbours.Add(GetNode(x, y - 1));
+                        neightbours.Add(GetNode(x, y + 1));
+                        neightbours.Add(GetNode(x - 1, y));
+                        neightbours.Add(GetNode(x + 1, y));
+
+                        nodes[x][y].SetConnectedNodes(neightbours.Where(x => x != null).ToArray());
+                        neightbours.Clear();
+                    }
+                }
+
+                IGridNode GetNode(int x, int y)
+                {
+                    if (y > 0)
+                        return nodes[y][x];
+
+                    if (x > 0)
+                        return nodes[y][x];
+
+                    if (x < settings.GridWidth - 1)
+                        return nodes[y][x];
+
+                    if (y < settings.GridHeight - 1)
+                        return nodes[y][x];
+                    return null;
+                }
+            }
+            void CreateNodes()
+            {
+                int counter = 0;
+                for (int y = 0; y < settings.GridHeight; y++)
+                {
+                    var row = nodes[y] = new GridNode[settings.GridWidth];
+                    for (int x = 0; x < settings.GridWidth; x++)
+                    {
+                        row[x] = new GridNode(settings.gridLayout[counter].weight, new(x, y));
+                        counter++;
+                    }
+                }
+            }
+
+
+            void Validate()
+            {
+                int gridCount = settings.GridWidth * settings.GridHeight;
+                if (gridCount != settings.gridLayout.Length) throw new Exception("layout and size do not match!");
+            }
 
         }
     }
