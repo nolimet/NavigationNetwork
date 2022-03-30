fbf9dd788376b08860f42bdd9e9b65f2485a6c04
Jesse Stam <jesse@jessestam.nl>
2022-03-30 08:36:58 +0200

    Added gridWorld Container

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace TowerDefence.World.Grid
+{
+    internal class GridWorld
+    {
+        private IEnumerable<IGridNode> world;
+        private readonly GridGenerator gridGenerator = new();
+
+        public void CreateWorld(GridSettings settings)
+        {
+            world = gridGenerator.CreateNodes(settings);
+        }
+
+        public IEnumerable<IGridNode> GetPath(IGridNode start, IGridNode end)
+        {
+
+            return world;
+        }
+    }
+}
