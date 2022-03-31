a4cfd5f37eda1f6abb8d0d9f29fedb9f221e87d6
Jesse Stam <jesse@jessestam.nl>
2022-03-31 17:46:13 +0200

    Created GridWorldSettings

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridWorldSettings.cs
@@ -0,0 +1,36 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TowerDefence.World.Grid
+{
+    [CreateAssetMenu(menuName = "Configuration/Grid World Settings", fileName = "Grid World Settings")]
+    public class GridWorldSettings : ScriptableObject
+    {
+        [SerializeField] private AssetReferenceT<Shader> tileShader;
+        [SerializeField] private AssetReferenceT<Texture2D> tileTexture;
+        [SerializeField] private Vector2 tileSize = Vector2.one;
+
+        public Vector2 TileSize => tileSize;
+
+        public async UniTask<Shader> GetTileShader()
+        {
+            if (!tileShader.IsDone)
+            {
+                var task = tileShader.LoadAssetAsync();
+                await task;
+                return task.Result;
+            }
+            else
+            {
+                return tileShader.Asset as Shader;
+            }
+        }
+
+        public async UniTask<Texture2D> GetTileTexture()
+        {
+            await tileTexture.LoadAssetAsync();
+            return tileTexture.Asset as Texture2D;
+        }
+    }
+}
