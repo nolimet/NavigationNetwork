df9d08a91ed08fe61aff968fb387141fd39762fa
Jesse Stam <jesse@jessestam.nl>
2022-04-01 17:54:12 +0200

    Upgrade grid world

--- TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridWorld.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridWorld.cs
@@ -1,20 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace TowerDefence.World.Grid
 {
     internal class GridWorld
     {
         private IEnumerable<IGridNode> world;
         private readonly GridGenerator gridGenerator = new();
+        private readonly GridVisualGenerator visualGenerator;
+        private readonly List<PathFinder> pathfinderPool = new();
 
-        public void CreateWorld(GridSettings settings)
+        public GridWorld(GridGenerator gridGenerator, GridVisualGenerator visualGenerator)
         {
-            world = gridGenerator.CreateNodes(settings);
+            this.gridGenerator = gridGenerator;
+            this.visualGenerator = visualGenerator;
         }
 
-        public IEnumerable<IGridNode> GetPath(IGridNode start, IGridNode end)
+        public async UniTask CreateWorld(GridSettings settings)
         {
+            world = gridGenerator.CreateNodes(settings);
+            await visualGenerator.CreateVisuals(world);
+        }
 
-            return world;
+        public async UniTask<IEnumerable<IGridNode>> GetPath(IGridNode start, IGridNode end)
+        {
+            PathFinder pathFinder;
+            IEnumerable<IGridNode> path;
+            if (pathfinderPool.Any(x => !x.Working))
+            {
+                pathFinder = pathfinderPool.First(x => !x.Working);
+                pathfinderPool.Remove(pathFinder);
+            }
+            else
+            {
+                pathFinder = new PathFinder();
+            }
+
+            await UniTask.SwitchToThreadPool();
+            path = pathFinder.GetPath(start, end);
+            await UniTask.SwitchToMainThread();
+
+            pathfinderPool.Add(pathFinder);
+            return path;
         }
     }
 }
