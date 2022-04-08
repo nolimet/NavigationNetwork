96b1593be31f88a8ef87dd87ef78db5f4e86caf3
Jesse Stam <jesse@jessestam.nl>
2022-04-08 08:36:21 +0200

    Made the gridnodes selectables

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/GridNodeSelector.cs
@@ -0,0 +1,32 @@
+﻿using DataBinding;
+using System.Collections.Generic;
+using System.Linq;
+using TowerDefence.Systems.Selection;
+using TowerDefence.Systems.Selection.Models;
+using UnityEngine;
+
+namespace TowerDefence.World.Grid
+{
+    internal class GridNodeSelector
+    {
+        private readonly ISelectionModel selectionModel;
+        private readonly BindingContext context = new(true);
+
+        public GridNodeSelector(ISelectionModel selectionModel)
+        {
+            this.selectionModel = selectionModel;
+
+            context.Bind(selectionModel, model => model.Selection, OnSelectionChanged);
+        }
+
+        private void OnSelectionChanged(IList<ISelectable> selection)
+        {
+            if (selection.Any(x => x is SelectableNode))
+            {
+                var selectedNode = selection.First(x => x is SelectableNode) as SelectableNode;
+
+                Debug.Log(selectedNode.GridNode.Position);
+            }
+        }
+    }
+}
---
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/SelectableNode.cs
@@ -0,0 +1,11 @@
+﻿using TowerDefence.Systems.Selection;
+using TowerDefence.World.Grid.Data;
+using UnityEngine;
+
+namespace TowerDefence.World.Grid
+{
+    internal class SelectableNode : MonoBehaviour, ISelectable
+    {
+        public IGridNode GridNode { get; set; }
+    }
+}
