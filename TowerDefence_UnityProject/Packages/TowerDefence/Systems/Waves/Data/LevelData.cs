3efad6240962b7989fb4d4d752ade0f9d27a0618
Jesse Stam <jesse@jessestam.nl>
2022-04-22 08:30:12 +0200

    Added gridsettings to LevelData

--- TowerDefence_UnityProject/Packages/TowerDefence/Systems/Waves/Data/LevelData.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/Waves/Data/LevelData.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using System;
+using TowerDefence.World.Grid.Data;
 using TowerDefence.World.Path.Data;
 
 namespace TowerDefence.Systems.Waves.Data
@@ -7,16 +8,27 @@ namespace TowerDefence.Systems.Waves.Data
     [Serializable]
     public readonly struct LevelData
     {
-        [JsonProperty("Waves", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("Waves", Required = Required.AllowNull, NullValueHandling = NullValueHandling.Ignore)]
         public readonly Wave[] waves;
 
-        [JsonProperty("Path", Required = Required.Always)]
-        public readonly PathData path;
+        [JsonProperty("Path", Required = Required.AllowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public readonly PathData? path;
 
-        public LevelData(Wave[] waves, PathData path)
+        [JsonProperty("gridData", Required = Required.AllowNull, NullValueHandling = NullValueHandling.Ignore)]
+        internal readonly GridSettings? gridSettings;
+
+        internal LevelData(Wave[] waves, PathData path)
         {
             this.waves = waves;
             this.path = path;
+            this.gridSettings = default;
+        }
+
+        internal LevelData(Wave[] waves, GridSettings gridSettings)
+        {
+            this.waves = waves;
+            this.gridSettings = gridSettings;
+            this.path = default;
         }
     }
 }
