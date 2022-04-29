2098fa020abdff5e4c25a34da5512ec0f8202d39
Jesse Stam <jesse@jessestam.nl>
2022-04-29 19:22:55 +0200

    Added debug code

--- TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/SelectableCell.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/World/Grid/SelectableCell.cs
@@ -7,5 +7,14 @@ namespace TowerDefence.World.Grid
     internal class SelectableCell : MonoBehaviour, ISelectable
     {
         public IGridCell GridNode { get; set; }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            foreach (var cell in GridNode.ConnectedCells)
+            {
+                Gizmos.DrawSphere(cell.WorldPosition, 0.5f);
+            }
+        }
     }
 }
