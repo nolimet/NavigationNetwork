79fbc26a4289ab67449edc7ed49542645ef51b63
Jesse Stam <jesse@jessestam.nl>
2022-05-05 18:34:27 +0200

    added util functions for selection

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Utility/SelectionUtility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TowerDefence.Entities.Components;
+using TowerDefence.Entities.Enemies;
+using TowerDefence.Entities.Towers;
+using TowerDefence.Systems.Selection;
+namespace TowerDefence.Utility
+{
+    internal static class SelectionUtility
+    {
+        public static bool TryFindTower(this IList<ISelectable> selection, out ITowerObject towerObject)
+        {
+            return selection.TryFindObject(out towerObject);
+        }
+
+        public static bool TryFindEnemy(this IList<ISelectable> selection, out IEnemyObject enemyObject)
+        {
+            return selection.TryFindObject(out enemyObject);
+        }
+
+        public static bool TryFindObject<T>(this IList<ISelectable> selection, out T selected) where T : class, ISelectable
+        {
+            if (selection.Any(x => x is T))
+            {
+                selected = selection.First(x => x is T) as T;
+                return true;
+            }
+
+            selected = null;
+            return false;
+        }
+
+        public static bool TryGetComponent<T>(this IList<IComponent> components, out T component) where T : class, IComponent
+        {
+            if (components.Any(x => x is T))
+            {
+                component = components.First(x => x is T) as T;
+                return true;
+            }
+
+            component = null;
+            return false;
+        }
+
+        public static T GetComponent<T>(this IList<IComponent> components) where T : class, IComponent
+        {
+            return components.FirstOrDefault(x => x is T) as T;
+        }
+    }
+}
