19d45184d4bca9e7242bb9b1fde45c8cde8ab9e6
Jesse Stam <jesse@jessestam.nl>
2022-05-05 18:35:32 +0200

    Added new controller to UIInstaller

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/UIInstaller.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/UIInstaller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using TowerDefence.UI.Health;
-using TowerDefence.UI.Tower;
+using TowerDefence.UI.Tower.Placing;
+using TowerDefence.UI.Tower.Range;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Zenject;
@@ -22,6 +23,7 @@ namespace TowerDefence.UI
         [SerializeField] private AssetReference hudControllerPrefab;
 
         [SerializeField] private AssetReference towerRangeDrawer;
+        [SerializeField] private AssetReference towerPlaceUI;
 
         public override void InstallBindings()
         {
@@ -36,15 +38,15 @@ namespace TowerDefence.UI
             Container.BindFactory<HealthDrawer, HealthDrawer.Factory>().FromComponentInNewPrefab(healthbarPrefab);
 
             Container.BindInterfacesAndSelfTo<TowerRangeDrawerController>().AsSingle().WithArguments(towerRangeDrawer).NonLazy();
-
+            Container.BindInterfacesAndSelfTo<TowerPlaceController>().AsSingle().WithArguments(towerPlaceUI).NonLazy();
             InstallAsync();
         }
 
         private async void InstallAsync()
         {
             await Task.Delay(100);
-            var aaa = await hudControllerPrefab.InstantiateAsync(Container.Resolve<UIContainer>().ScreenUIContainer, false);
-            Container.InjectGameObject(aaa as GameObject);
+            var hudController = await this.hudControllerPrefab.InstantiateAsync(Container.Resolve<UIContainer>().ScreenUIContainer, false);
+            Container.InjectGameObject(hudController as GameObject);
         }
     }
 }
