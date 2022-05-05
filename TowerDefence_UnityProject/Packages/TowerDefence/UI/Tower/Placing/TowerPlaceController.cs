1d20f2593f878eb2a34a3bbe522b6a4d5b605dc5
Jesse Stam <jesse@jessestam.nl>
2022-05-05 18:34:47 +0200

    Added partial TowerPlaceController

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Tower/Placing/TowerPlaceController.cs
@@ -0,0 +1,32 @@
+﻿using DataBinding;
+using System.Collections.Generic;
+using TowerDefence.Systems.Selection;
+using TowerDefence.Systems.Selection.Models;
+using TowerDefence.Utility;
+using TowerDefence.World.Grid;
+
+namespace TowerDefence.UI.Tower.Placing
+{
+    internal class TowerPlaceController
+    {
+        private readonly BindingContext bindingContext = new(true);
+
+        public TowerPlaceController(ISelectionModel selectionModel)
+        {
+            bindingContext.Bind(selectionModel, x => x.Selection, OnSelectionChanged);
+        }
+
+        private void OnSelectionChanged(IList<ISelectable> selection)
+        {
+            if (selection.TryFindObject<SelectableCell>(out var cell))
+            {
+
+            }
+        }
+
+        ~TowerPlaceController()
+        {
+            bindingContext.Dispose();
+        }
+    }
+}
