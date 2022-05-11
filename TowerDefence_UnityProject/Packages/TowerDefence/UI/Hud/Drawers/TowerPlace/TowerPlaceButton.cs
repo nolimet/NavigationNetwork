5eb6e061ca3c6b41af755e9046b7c843cb05bef8
Jesse Stam <jesse@jessestam.nl>
2022-05-11 18:52:39 +0200

    Added tower place hud button

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Hud/Drawers/TowerPlace/TowerPlaceButton.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.UI.Hud.PlaceTower
+{
+    internal class TowerPlaceButton : MonoBehaviour
+    {
+        public delegate void CallbackDelegate(string towerId);
+        private CallbackDelegate callback;
+        private string towerId;
+        [SerializeField] private TextMeshProUGUI label;
+
+        public void OnClicked()
+        {
+            callback(towerId);
+        }
+
+        public class Factory : PlaceholderFactory<TowerPlaceButton>
+        {
+            public TowerPlaceButton Create(Transform container, string labelText, string towerId, CallbackDelegate onClick)
+            {
+                var towerPlaceButton = base.Create();
+                towerPlaceButton.transform.SetParent(container);
+
+                towerPlaceButton.label.text = labelText;
+                towerPlaceButton.callback = onClick;
+                towerPlaceButton.towerId = towerId;
+
+                return towerPlaceButton;
+            }
+        }
+    }
+}
