d952b22c88df3d75e213c653f59633b0d530d2ce
Jesse Stam <jesse@jessestam.nl>
2022-05-11 18:47:24 +0200

    Update hud installer

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Hud/HudInstaller.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Hud/HudInstaller.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using TowerDefence.UI.Hud.PlaceTower;
+using TowerDefence.UI.Hud.SubControllers;
+using UnityEngine;
 using Zenject;
 
 namespace TowerDefence.UI.Hud
 {
     public class HudInstaller : MonoInstaller
     {
-        [SerializeField]
-        private TowerHudDrawer towerHud;
-
+        [SerializeField] private TowerHudDrawer towerHud;
+        [SerializeField] private TowerPlaceHudDrawer towerPlaceHud;
+        [SerializeField] private TowerPlaceButton towerPlaceButtonPrefab;
         public override void InstallBindings()
         {
+            Container.BindInterfacesAndSelfTo<TowerPlaceController>().AsCached().NonLazy();
+            Container.BindFactory<TowerPlaceButton, TowerPlaceButton.Factory>().FromComponentInNewPrefab(towerPlaceButtonPrefab);
+
+            Container.BindInstance(towerPlaceHud).AsCached();
             Container.BindInstance(towerHud).AsCached();
         }
     }
