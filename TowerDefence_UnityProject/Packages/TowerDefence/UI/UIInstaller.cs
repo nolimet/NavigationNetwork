f3efb8c217db0d5aab9f7eed98b88db43901569a
Jesse Stam <jesse@jessestam.nl>
2022-05-11 18:46:02 +0200

    Moved towerplace UI into the hud continer

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/UIInstaller.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/UIInstaller.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using TowerDefence.UI.Health;
-using TowerDefence.UI.Tower.Placing;
 using TowerDefence.UI.Tower.Range;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -21,9 +20,7 @@ namespace TowerDefence.UI
         private GameObject screenUIContainerPrefab;
 
         [SerializeField] private AssetReference hudControllerPrefab;
-
         [SerializeField] private AssetReference towerRangeDrawer;
-        [SerializeField] private AssetReference towerPlaceUI;
 
         public override void InstallBindings()
         {
@@ -38,15 +35,15 @@ namespace TowerDefence.UI
             Container.BindFactory<HealthDrawer, HealthDrawer.Factory>().FromComponentInNewPrefab(healthbarPrefab);
 
             Container.BindInterfacesAndSelfTo<TowerRangeDrawerController>().AsSingle().WithArguments(towerRangeDrawer).NonLazy();
-            Container.BindInterfacesAndSelfTo<TowerPlaceController>().AsSingle().WithArguments(towerPlaceUI).NonLazy();
+
             InstallAsync();
         }
 
         private async void InstallAsync()
         {
             await Task.Delay(100);
-            var hudController = await this.hudControllerPrefab.InstantiateAsync(Container.Resolve<UIContainer>().ScreenUIContainer, false);
-            Container.InjectGameObject(hudController as GameObject);
+            var hudPrefab = await this.hudControllerPrefab.LoadAssetAsync<GameObject>() as GameObject;
+            Container.InstantiatePrefab(hudPrefab, Container.Resolve<UIContainer>().ScreenUIContainer);
         }
     }
 }
