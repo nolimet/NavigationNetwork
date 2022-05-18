85e3b071c498e4c265b9b0c1319df08dffd69737
Jesse Stam <jesse@jessestam.nl>
2022-05-18 08:36:56 +0200

    Added first itteration of world loader

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/WorldLoad/WorldLoadController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using TowerDefence.Systems.Waves;
+using TowerDefence.Systems.Waves.Data;
+using TowerDefence.World.Grid;
+using UnityEngine;
+
+namespace TowerDefence.Packages.TowerDefence.Systems.WorldLoad
+{
+    internal class WorldLoadController
+    {
+        public enum LevelType
+        {
+            json,
+            lvl
+        }
+
+        private readonly GridWorld gridWorld;
+        private readonly WaveController waveController;
+
+        public string[] GetLevels()
+        {
+            //TODO get all levels
+            throw new NotImplementedException("TODO Implement getting all levels");
+        }
+
+        public async void LoadLevel(string level, LevelType type)
+        {
+            var file = new FileInfo(FormatWorldName());
+            if (file.Exists)
+            {
+                string json;
+                using (var reader = file.OpenText())
+                {
+                    json = reader.ReadToEnd();
+                }
+                var lvlData = JsonConvert.DeserializeObject<LevelData>(json);
+
+                if (lvlData.gridSettings.HasValue)
+                    await gridWorld.CreateWorld(lvlData.gridSettings.Value);
+                else
+                    Debug.LogError("There where on grid settings");
+
+                waveController.SetWaves(lvlData.waves);
+            }
+            string FormatWorldName()
+            {
+                return Path.Combine(Application.streamingAssetsPath, "Levels", $"{level}.{type}");
+            }
+        }
+    }
+}
