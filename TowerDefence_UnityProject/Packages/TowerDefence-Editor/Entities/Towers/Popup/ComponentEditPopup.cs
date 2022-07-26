772111d481183159b73a3f9c1a1c69aaf4a9cd21
Jesse Stam <jesse@jessestam.nl>
2022-07-26 18:37:21 +0200

    Heavily changed how componets are drawn.
    Now introducing reflection o.o spooky shit

--- TowerDefence_UnityProject/Packages/TowerDefence-Editor/Entities/Towers/Popup/ComponentEditPopup.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence-Editor/Entities/Towers/Popup/ComponentEditPopup.cs
@@ -1,4 +1,8 @@
-﻿using UnityEditor;
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace TowerDefence.Entities.Towers.Popup
@@ -6,12 +10,19 @@ namespace TowerDefence.Entities.Towers.Popup
     internal class ComponentEditPopup : PopupWindowContent
     {
         public readonly DisplayData displayData;
-        private string jsondata;
+        private readonly FieldInfo[] fields;
 
         public ComponentEditPopup(DisplayData displayData)
         {
             this.displayData = displayData;
-            jsondata = displayData.displayJson;
+
+            var jsonPropertyType = typeof(JsonPropertyAttribute);
+            var component = displayData.component.GetType();
+            var f1 = component.GetFields(BindingFlags.Instance | BindingFlags.Public);
+            var f2 = component.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            var allFields = f1.Concat(f2);
+
+            fields = allFields.Where(x => x.CustomAttributes.Any(c => c.AttributeType == jsonPropertyType)).ToArray();
         }
 
         public override void OnGUI(Rect rect)
@@ -20,8 +31,7 @@ namespace TowerDefence.Entities.Towers.Popup
             {
                 if (GUILayout.Button("Save"))
                 {
-                    displayData.displayJson = jsondata;
-                    displayData.ComponentFromJson();
+                    displayData.ComponentToJson();
                     displayData.UpdateTowerComponentData();
 
                     editorWindow.Close();
@@ -34,7 +44,40 @@ namespace TowerDefence.Entities.Towers.Popup
                 GUILayout.FlexibleSpace();
             }
 
-            jsondata = EditorGUILayout.TextArea(jsondata);
+            DrawField();
+        }
+
+        private void DrawField()
+        {
+            foreach (var field in fields)
+            {
+                var val = field.GetValue(displayData.component);
+                if (val is null)
+                {
+                    var type = field.FieldType;
+                    if (type.IsValueType)
+                    {
+                        val = Activator.CreateInstance(field.FieldType);
+                    }
+                    else if (type == typeof(string))
+                    {
+                        val = string.Empty;
+                    }
+                }
+                val = val switch
+                {
+                    bool b => EditorGUILayout.Toggle(field.Name, b),
+                    float f => EditorGUILayout.FloatField(field.Name, f),
+                    double d => EditorGUILayout.DoubleField(field.Name, d),
+                    string s => EditorGUILayout.TextField(field.Name, s),
+                    int i => EditorGUILayout.IntField(field.Name, i),
+                    long l => EditorGUILayout.LongField(field.Name, l),
+                    Color c => EditorGUILayout.ColorField(field.Name, c),
+                    _ => val,
+                };
+
+                field.SetValue(displayData.component, val);
+            }
         }
     }
 }
