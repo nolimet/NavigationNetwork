2413b7d1797c154681bddf9ecfc147aeb6e17190
Jesse Stam <jesse@jessestam.nl>
2022-08-04 22:05:05 +0200

    Updated some code

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Menu/LevelDisplay/LevelDisplayController.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Menu/LevelDisplay/LevelDisplayController.cs
@@ -1,7 +1,11 @@
 ﻿using DataBinding;
+using NoUtil.Extentsions;
 using System;
+using System.Collections.Generic;
 using TowerDefence.Systems.WorldLoad;
 using TowerDefence.UI.Models;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace TowerDefence.UI.Menu.LevelDisplay
 {
@@ -9,12 +13,30 @@ namespace TowerDefence.UI.Menu.LevelDisplay
     {
         private readonly WorldLoadController worldLoadController;
         private readonly BindingContext bindingContext = new(true);
+        private UIDocumentContainer documentContainer;
 
         public LevelDisplayController(WorldLoadController worldLoadController, IUIContainers uiContainers)
         {
             this.worldLoadController = worldLoadController;
 
-            //bindingContext.Bind<IUIContainers>(uiContainers, x => x.UIContainers);
+            bindingContext.Bind(uiContainers, x => x.Containers, OnContainersChanged);
+        }
+
+        private void OnContainersChanged(IList<IUIContainer> obj)
+        {
+            if (obj.TryFind(x => x.Name == "Main", out var container) && container is UIDocumentContainer documentContainer)
+            {
+                this.documentContainer = documentContainer;
+                UpdateUI();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            var document = documentContainer.Document.rootVisualElement;
+            var levelsContainer = document.Q("Levels")?.Q("unity-content-container");
+
+            Debug.Log(levelsContainer);
         }
 
         public void Dispose() => bindingContext.Dispose();
