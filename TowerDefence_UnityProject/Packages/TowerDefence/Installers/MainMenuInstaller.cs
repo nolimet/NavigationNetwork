6dc7c546aa756a98c4627f9b021a127ad9e5c36d
Jesse Stam <nolimet@stamdata.nl>
2022-08-04 23:03:21 +0200

    added installer for main menu

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Installers/MainMenuInstaller.cs
@@ -0,0 +1,13 @@
+﻿using TowerDefence.UI.Menu.LevelDisplay;
+using Zenject;
+
+namespace TowerDefence.Installers
+{
+    public class MainMenuInstaller : MonoInstaller<MainMenuInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<LevelDisplayController>().AsSingle().NonLazy();
+        }
+    }
+}
