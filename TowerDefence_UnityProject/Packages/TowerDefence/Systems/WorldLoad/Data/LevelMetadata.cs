6e3e3a4c98224965faf9e779190592c5b5ee86d6
Jesse Stam <jesse@jessestam.nl>
2022-08-05 16:58:57 +0200

    Added crude version of metadata

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/WorldLoad/Data/LevelMetadata.cs
@@ -0,0 +1,14 @@
+﻿namespace TowerDefence.Systems.WorldLoader.Data
+{
+    public readonly struct LevelMetadata
+    {
+        public readonly string DisplayName;
+        public readonly string RelativeLevelPath;
+
+        public LevelMetadata(string displayName, string relativeLevelPath)
+        {
+            DisplayName = displayName;
+            RelativeLevelPath = relativeLevelPath;
+        }
+    }
+}
