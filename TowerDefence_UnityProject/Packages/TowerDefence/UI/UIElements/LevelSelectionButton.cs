51f677e1aac1c73cb0e2b77a3d13c6588a18e07b
Jesse Stam <jesse@jessestam.nl>
2022-08-10 08:28:12 +0200

    Fixed button name and added class assignment

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/UIElements/LevelSelectionButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+namespace TowerDefence.UI.UIElements
+{
+    public class LevelSelectionButton : Button
+    {
+        public new readonly string ussClassName = "level-selection-button";
+        public event Action<string> callback;
+        public string callbackValue = "";
+
+        public LevelSelectionButton() : base()
+        {
+            base.clicked += OnClicked;
+            AddToClassList(ussClassName);
+        }
+
+        private void OnClicked()
+        {
+            callback?.Invoke(callbackValue);
+        }
+    }
+}
