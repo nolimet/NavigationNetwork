882e9145ecd3d68d1656dfe18e8cfcbb96c34f61
jesse stam <nolimet@stamdata.nl>
2022-08-14 11:01:19 +0200

    Disabled old path stuff  from example

--- TowerDefence_UnityProject/Assets/Examples/ExampleEditor/Paths/ExamplePathGeneratorEditor.cs
+++ TowerDefence_UnityProject/Assets/Examples/ExampleEditor/Paths/ExamplePathGeneratorEditor.cs
@@ -10,40 +10,44 @@ namespace TowerDefence.Examples.Paths
         {
             base.OnInspectorGUI();
             var target = this.target as ExamplePathGenerator;
-            if (!target) { return; }
+            if (!target)
+            {
+                return;
+            }
+
             using (var disableScope = new EditorGUI.DisabledGroupScope(!Application.isPlaying))
             {
-                using (new EditorGUILayout.HorizontalScope())
-                {
-                    GUILayout.FlexibleSpace();
-                    if (GUILayout.Button("Build Path"))
-                    {
-                        target.BuildExample();
-                    }
-
-                    if (GUILayout.Button("Generate Path File"))
-                    {
-                        target.GenerateExamplePath();
-                    }
-
-                    using (var disableScope2 = new EditorGUI.DisabledGroupScope(target.ConstructedPath == null))
-                    {
-                        if (GUILayout.Button("Create Walker"))
-                        {
-                            target.CreateWalker();
-                        }
-
-                        if (GUILayout.Button("Create 20 walkers"))
-                        {
-                            for (int i = 0; i < 20; i++)
-                            {
-                                target.CreateWalkerDelayed(Random.Range(0, 1f));
-                            }
-                        }
-                    }
-
-                    GUILayout.FlexibleSpace();
-                }
+                //     using (new EditorGUILayout.HorizontalScope())
+                //     {
+                //         GUILayout.FlexibleSpace();
+                //         if (GUILayout.Button("Build Path"))
+                //         {
+                //             target.BuildExample();
+                //         }
+                //
+                //         if (GUILayout.Button("Generate Path File"))
+                //         {
+                //             target.GenerateExamplePath();
+                //         }
+                //
+                //         using (var disableScope2 = new EditorGUI.DisabledGroupScope(target.ConstructedPath == null))
+                //         {
+                //             if (GUILayout.Button("Create Walker"))
+                //             {
+                //                 target.CreateWalker();
+                //             }
+                //
+                //             if (GUILayout.Button("Create 20 walkers"))
+                //             {
+                //                 for (int i = 0; i < 20; i++)
+                //                 {
+                //                     target.CreateWalkerDelayed(Random.Range(0, 1f));
+                //                 }
+                //             }
+                //         }
+                //
+                //         GUILayout.FlexibleSpace();
+                //     }
 
                 using (new EditorGUILayout.HorizontalScope())
                 {
@@ -56,33 +60,36 @@ namespace TowerDefence.Examples.Paths
                     {
                         target.CreateGridWalker();
                     }
+
                     GUILayout.FlexibleSpace();
                 }
             }
-            if (target.ConstructedPath != null && Application.isPlaying)
-            {
-                var paths = target.ConstructedPath.paths;
 
-                foreach (var path in paths)
+            if (target.ConstructedPath == null || !Application.isPlaying) return;
+            var paths = target.ConstructedPath.paths;
+
+            foreach (var path in paths)
+            {
+                using (var h1 = new EditorGUILayout.HorizontalScope())
                 {
-                    using (var h1 = new EditorGUILayout.HorizontalScope())
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("DumpCurve Keys"))
                     {
-                        GUILayout.FlexibleSpace();
-                        if (GUILayout.Button("DumpCurve Keys"))
-                        {
-                            path.Value.LogCurveValues();
-                        }
-                        GUILayout.FlexibleSpace();
+                        path.Value.LogCurveValues();
                     }
-                    EditorGUILayout.LabelField($"path {path.Key}");
-                    using (var indent = new EditorGUI.IndentLevelScope(1))
-                    {
-                        EditorGUILayout.CurveField("X", path.Value.curveX);
-                        EditorGUILayout.CurveField("Y", path.Value.curveY);
-                        EditorGUILayout.CurveField("Z", path.Value.curveZ);
-                    }
-                    EditorGUILayout.Space();
+
+                    GUILayout.FlexibleSpace();
                 }
+
+                EditorGUILayout.LabelField($"path {path.Key}");
+                using (var indent = new EditorGUI.IndentLevelScope(1))
+                {
+                    EditorGUILayout.CurveField("X", path.Value.curveX);
+                    EditorGUILayout.CurveField("Y", path.Value.curveY);
+                    EditorGUILayout.CurveField("Z", path.Value.curveZ);
+                }
+
+                EditorGUILayout.Space();
             }
         }
     }
