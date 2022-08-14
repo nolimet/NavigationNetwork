48a68935f8a4c79bf10dd8a8975149ec1e2e496f
jesse stam <nolimet@stamdata.nl>
2022-08-14 11:00:33 +0200

    Added a method to clear the UIContainers

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/UIContainer.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/UIContainer.cs
@@ -5,13 +5,11 @@ namespace TowerDefence.UI
 {
     public class UIContainer : MonoBehaviour
     {
-        [SerializeField, ReadOnly]
-        private Transform worldUIContainer;
+        [SerializeField, ReadOnly] private Transform worldUIContainer;
 
         public Transform WorldUIContainer => worldUIContainer;
 
-        [SerializeField, ReadOnly]
-        private Transform screenUIContainer;
+        [SerializeField, ReadOnly] private Transform screenUIContainer;
 
         public Transform ScreenUIContainer => screenUIContainer;
 
@@ -19,6 +17,19 @@ namespace TowerDefence.UI
         {
             this.worldUIContainer = worldUIContainer;
             this.screenUIContainer = screenUIContainer;
+        }
+
+        public void ClearContainers()
+        {
+            foreach (Transform child in worldUIContainer)
+            {
+                Debug.Log(child.name);
+            }
+
+            foreach (Transform child in screenUIContainer)
+            {
+                Debug.Log(child.name);
+            }
         }
     }
 }
