2e7e0ffdd2a850e505da8624c24c1fea9318f819
Jesse Stam <nolimet@stamdata.nl>
2022-08-16 08:14:15 +0200

    Added debug Actions

--- TowerDefence_UnityProject/Packages/TowerDefence/Input/InputInstaller.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Input/InputInstaller.cs
@@ -9,6 +9,7 @@ namespace TowerDefence.Input
         public override void InstallBindings()
         {
             Container.Bind<SelectionInputActions>().AsSingle();
+            Container.Bind<DebugActions>().AsSingle();
         }
     }
 }
