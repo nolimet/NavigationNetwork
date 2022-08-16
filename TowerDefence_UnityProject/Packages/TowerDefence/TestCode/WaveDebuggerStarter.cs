760a9f81c416dfb6e8edb0bf6ec95b168b87adde
Jesse Stam <nolimet@stamdata.nl>
2022-08-16 08:15:18 +0200

    Added some test code to start a wave

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/TestCode/WaveDebuggerStarter.cs
@@ -0,0 +1,33 @@
+﻿using TowerDefence.Input;
+using TowerDefence.Systems.Waves;
+using UnityEngine.InputSystem;
+
+namespace TowerDefence.TestCode
+{
+    public class WaveDebuggerStarter
+    {
+        private readonly DebugActions debugActions;
+        private readonly WaveController waveController;
+
+        public WaveDebuggerStarter(DebugActions debugActions, WaveController waveController)
+        {
+            this.debugActions = debugActions;
+            this.waveController = waveController;
+
+            debugActions.Waves.StartWaves.performed += OnStartWavePressed;
+            debugActions.Waves.StartWaves.Enable();
+            debugActions.Waves.Enable();
+            debugActions.Enable();
+        }
+
+        ~WaveDebuggerStarter()
+        {
+            debugActions.Waves.StartWaves.performed -= OnStartWavePressed;
+        }
+
+        private void OnStartWavePressed(InputAction.CallbackContext obj)
+        {
+            waveController.StartWavePlayBack();
+        }
+    }
+}
