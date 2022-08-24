e04fcca747c488a0d662985f1df351e31ff33bfc
Jesse Stam <jesse@jessestam.nl>
2022-08-24 08:25:28 +0200

    Added something to help with scene loading

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/SceneLoading/SceneLoadingInstaller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace TowerDefence.Packages.TowerDefence.SceneLoading
+{
+    public class SceneLoadingInstaller : MonoInstaller
+    {
+        [SerializeField] private SceneReferences sceneReferences;
+        public override void InstallBindings()
+        {
+            Container.BindInstance(sceneReferences).AsSingle();
+        }
+    }
+}
