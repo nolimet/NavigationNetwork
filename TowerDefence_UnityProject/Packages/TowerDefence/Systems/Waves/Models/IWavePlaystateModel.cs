6a86bf6b4980b172c72d62f7be148d61ec29dc2e
Jesse Stam <jesse@jessestam.nl>
2022-10-26 08:25:41 +0200

    Added a model for keep tracking of the state of the waves

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/Waves/Models/IWavePlaystateModel.cs
@@ -0,0 +1,16 @@
+﻿using DataBinding.BaseClasses;
+using DataBinding.Helpers;
+
+namespace TowerDefence.Systems.Waves.Models
+{
+    [DataModel(Shared = true, AddToZenject = true)]
+    public interface IWavePlayStateModel : IModelBase
+    {
+        bool wavesPlaying { get; set; }
+        bool autoPlayEnabled { get; set; }
+
+        int wavesLeft { get; set; }
+        int totalWaves { get; set; }
+        int activeWave { get; set; }
+    }
+}
