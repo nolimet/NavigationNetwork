43d5e48f0a07e7c78f23920a06ff5bd89ef98bf0
Jesse Stam <jesse@jessestam.nl>
2022-11-07 08:51:12 +0100

    Added most of the logic needed

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/Drawers/TowerPlaceController.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/Drawers/TowerPlaceController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using DataBinding;
 using NoUtil.Extentsions;
+using TowerDefence.Systems.Selection;
+using TowerDefence.Systems.Selection.Models;
 using TowerDefence.UI.Models;
+using TowerDefence.World.Grid;
 using UnityEngine.UIElements;
 
 namespace TowerDefence.UI.Game.Hud.Drawers
 {
-    public class TowerPlacerController
+    public class TowerPlaceController
     {
         private const string containerId = "game_ui";
         private const string towerPlaceContainerId = "places_tower_container";
@@ -15,9 +18,24 @@ namespace TowerDefence.UI.Game.Hud.Drawers
         private IUIContainer activeContainer;
         private VisualElement towerPlaceContainer;
 
-        public TowerPlacerController(IUIContainers uiContainers)
+        public TowerPlaceController(IUIContainers uiContainers, ISelectionModel selectionModel)
         {
             bindingContext.Bind(uiContainers, x => x.Containers, OnUIContainersChanged);
+            bindingContext.Bind(selectionModel, x => x.Selection, OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged(IList<ISelectable> selection)
+        {
+            if (selection.Count == 1 && selection.TryFind(x => x is SelectableCell, out var s) && s is SelectableCell cell && !cell.GridCell.HasStructure)
+            {
+                towerPlaceContainer.visible = true;
+                towerPlaceContainer.SetEnabled(true);
+            }
+            else
+            {
+                towerPlaceContainer.visible = false;
+                towerPlaceContainer.SetEnabled(false);
+            }
         }
 
         private void OnUIContainersChanged(IList<IUIContainer> uiContainers)
