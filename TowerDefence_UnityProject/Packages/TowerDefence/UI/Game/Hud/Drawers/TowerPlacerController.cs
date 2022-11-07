42dc80442812480ebf6aef5014e5bcc902800f05
Jesse Stam <jesse@jessestam.nl>
2022-11-07 08:42:29 +0100

    Added new UI parts

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/Drawers/TowerPlacerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using DataBinding;
+using NoUtil.Extentsions;
+using TowerDefence.UI.Models;
+using UnityEngine.UIElements;
+
+namespace TowerDefence.UI.Game.Hud.Drawers
+{
+    public class TowerPlacerController
+    {
+        private const string containerId = "game_ui";
+        private const string towerPlaceContainerId = "places_tower_container";
+
+        private readonly BindingContext bindingContext = new();
+        private IUIContainer activeContainer;
+        private VisualElement towerPlaceContainer;
+
+        public TowerPlacerController(IUIContainers uiContainers)
+        {
+            bindingContext.Bind(uiContainers, x => x.Containers, OnUIContainersChanged);
+        }
+
+        private void OnUIContainersChanged(IList<IUIContainer> uiContainers)
+        {
+            if (activeContainer is not null)
+            {
+                UnBind();
+            }
+
+            if (uiContainers.TryFind(x => x.Name == containerId, out var container) && container is UIDocumentContainer documentContainer)
+            {
+                var root = documentContainer.Document.rootVisualElement;
+                towerPlaceContainer = root.Q(towerPlaceContainerId);
+            }
+
+            void UnBind()
+            {
+            }
+        }
+    }
+}
---
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/HudInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace TowerDefence.UI.Game.Hud
+{
+    public class HudInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
