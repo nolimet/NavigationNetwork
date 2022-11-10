9963d89c63db0616bc6279305911464a4c55406c
Jesse Stam <jesse@jessestam.nl>
2022-11-10 08:17:00 +0100

    Reworked tower place controller to work with ui toolkit

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/Controllers/TowerPlaceController.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/Hud/Controllers/TowerPlaceController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
 using DataBinding;
 using NoUtil.Extentsions;
+using TowerDefence.Entities.Towers;
 using TowerDefence.Systems.Selection;
 using TowerDefence.Systems.Selection.Models;
+using TowerDefence.UI.Game.Hud.CustomUIElements;
 using TowerDefence.UI.Models;
 using TowerDefence.World.Grid;
 using UnityEngine.UIElements;
@@ -15,11 +19,19 @@ namespace TowerDefence.UI.Game.Hud.Drawers
         private const string towerPlaceContainerId = "places_tower_container";
 
         private readonly BindingContext bindingContext = new();
+        private readonly TowerConfigurationData towerConfigurationData;
+        private readonly TowerService towerService;
+
         private IUIContainer activeContainer;
+        private ISelectionModel selectionModel;
         private VisualElement towerPlaceContainer;
 
-        public TowerPlaceController(IUIContainers uiContainers, ISelectionModel selectionModel)
+        public TowerPlaceController(IUIContainers uiContainers, ISelectionModel selectionModel, TowerService towerService, TowerConfigurationData towerConfigurationData)
         {
+            this.towerService = towerService;
+            this.towerConfigurationData = towerConfigurationData;
+            this.selectionModel = selectionModel;
+
             bindingContext.Bind(uiContainers, x => x.Containers, OnUIContainersChanged);
             bindingContext.Bind(selectionModel, x => x.Selection, OnSelectionChanged);
         }
@@ -51,10 +63,35 @@ namespace TowerDefence.UI.Game.Hud.Drawers
             {
                 var root = documentContainer.Document.rootVisualElement;
                 towerPlaceContainer = root.Q(towerPlaceContainerId);
+
+                var towers = towerConfigurationData.Towers.Keys;
+                foreach (var towerId in towers)
+                {
+                    var towerButton = new TowerPlaceButton(towerId);
+                    towerButton.OnCallback += OnTowerPlaceButtonClicked;
+                    towerButton.AddToClassList("HUD-Button");
+                    towerButton.AddToClassList("HUD-Text");
+                    towerPlaceContainer.Add(towerButton);
+                }
             }
 
             void UnBind()
             {
+                if (towerPlaceContainer is null) return;
+
+                foreach (var child in towerPlaceContainer.Children().Where(x => x is TowerPlaceButton))
+                {
+                    child.RemoveFromHierarchy();
+                }
+            }
+        }
+
+        private void OnTowerPlaceButtonClicked(string towerId)
+        {
+            if (selectionModel.Selection.Any(x => x is SelectableCell))
+            {
+                var cell = selectionModel.Selection.First(x => x is SelectableCell) as SelectableCell;
+                towerService.PlaceTower(towerId, cell!.GridCell.WorldPosition, cell.GridCell).Forget();
             }
         }
     }
