c8fa579a546a5603de617ac63235f8d4fb0b35bd
Jesse Stam <nolimet@stamdata.nl>
2022-11-10 21:06:42 +0100

    Added tower Visual settings to add sprites to the twoer

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/BaseComponents/TowerVisualSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TowerDefence.Entities.Components;
+using TowerDefence.Entities.Towers.Components.Interfaces;
+using TowerDefence.Entities.Towers.Models;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TowerDefence.Entities.Towers.Components.BaseComponents
+{
+    [Serializable, Component(ComponentType.Tower, AllowMultiple = false)]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class TowerVisualSettings : IComponent, IAsyncInitializer, IDisposable
+    {
+        [JsonProperty] [SerializeField] private AssetReferenceT<Sprite> towerSprite;
+
+
+        public async Task AsyncPostInit(ITowerObject towerObject, ITowerModel towerModel)
+        {
+            var spriteRenderer = towerObject.Transform.GetComponent<SpriteRenderer>();
+
+            var loadTask = towerSprite.LoadAssetAsync();
+            if (!loadTask.IsDone) await loadTask;
+
+            if (towerSprite.IsValid()) spriteRenderer.sprite = loadTask.Result;
+        }
+
+        public void Dispose()
+        {
+            towerSprite.ReleaseAsset();
+        }
+    }
+}
