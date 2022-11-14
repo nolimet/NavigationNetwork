bd2486ea3c284eff82b187768a51f0fb6735798e
Jesse Stam <jesse@jessestam.nl>
2022-11-14 08:51:50 +0100

    Added code return to the main menu

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/PauseMenu/PauseMenuController.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Game/PauseMenu/PauseMenuController.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DataBinding;
 using TowerDefence.Input;
+using TowerDefence.Packages.TowerDefence.SceneLoading;
 using TowerDefence.UI.Models;
-using UnityEngine;
+using TowerDefence.World.Grid;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 namespace TowerDefence.UI.Game.PauseMenu
 {
-    public class PauseMenuController
+    public sealed class PauseMenuController : IDisposable
     {
         private const string GameHUDContainerId = "GameUI-HUD";
         private const string PauseMenuId = "PopupMenu";
+        private const string BackToMenuId = "BackToMenu";
+
 
         private readonly IUIContainers uiContainers;
         private readonly BindingContext bindingContext = new();
         private readonly UIInputActions uiInputActions;
+        private readonly SceneReferences sceneReferences;
+        private readonly GridWorld gridWorld;
 
         private VisualElement pauseMenu;
+        private Button returnToMenuButton;
 
-        public PauseMenuController(IUIContainers uiContainers, UIInputActions uiInputActions)
+        internal PauseMenuController(IUIContainers uiContainers, UIInputActions uiInputActions, SceneReferences sceneReferences, GridWorld gridWorld)
         {
-            Debug.Log("Bleep bloop");
             this.uiContainers = uiContainers;
             this.uiInputActions = uiInputActions;
+            this.sceneReferences = sceneReferences;
+            this.gridWorld = gridWorld;
 
             bindingContext.Bind(uiContainers, x => x.Containers, OnUIContainerChanged);
 
@@ -50,14 +58,36 @@ namespace TowerDefence.UI.Game.PauseMenu
             void Bind()
             {
                 if (!uiContainers.TryGetContainer(GameHUDContainerId, out UIDocumentContainer uiDocumentContainer)) return;
+
                 var root = uiDocumentContainer.Document.rootVisualElement;
 
                 pauseMenu = root.Q(PauseMenuId);
+                returnToMenuButton = root.Q<Button>(BackToMenuId);
+                returnToMenuButton.clicked += OnReturnToMenuClicked;
             }
 
             void UnBind()
             {
+                if (returnToMenuButton is not null)
+                {
+                    returnToMenuButton.clicked -= OnReturnToMenuClicked;
+                }
             }
+        }
+
+        private async void OnReturnToMenuClicked()
+        {
+            returnToMenuButton.SetEnabled(false);
+            uiInputActions.Main.OpenPauseMenu.Disable();
+
+            gridWorld.DestroyWorld();
+
+            await sceneReferences.MainMenuScene.LoadSceneAsync();
+        }
+
+        public void Dispose()
+        {
+            bindingContext?.Dispose();
         }
     }
 }
