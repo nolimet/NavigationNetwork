74751591efec33ba3df6ea1ad6e5fd9c6314168f
Jesse Stam <jesse@jessestam.nl>
2022-11-29 08:51:10 +0100

    Updated UI Document container for a new class

--- TowerDefence_UnityProject/Packages/TowerDefence/UI/Containers/UIDocumentContainer.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/UI/Containers/UIDocumentContainer.cs
@@ -3,24 +3,30 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Zenject;
 
-namespace TowerDefence.UI
+namespace TowerDefence.UI.Containers
 {
-    internal sealed class UIDocumentContainer : MonoBehaviour, IUIContainer
+    internal class UIDocumentContainer : MonoBehaviour, IUIContainer
     {
-        private IUIContainers containers;
+        protected IUIContainers containers;
 
-        [field: SerializeField] public UIDocument Document { get; private set; }
+        [field: SerializeField] public UIDocument Document { get; protected set; }
 
         [field: SerializeField] public string Name { get; private set; }
+
+        public virtual VisualElement VisualRoot => Document.rootVisualElement;
 
         [Inject]
         public void Inject(IUIContainers containers)
         {
             this.containers = containers;
-            this.containers.Containers.Add(this);
         }
 
-        private void OnDestroy()
+        protected virtual void Start()
+        {
+            containers.Containers.Add(this);
+        }
+
+        protected virtual void OnDestroy()
         {
             containers.Containers.Remove(this);
         }
