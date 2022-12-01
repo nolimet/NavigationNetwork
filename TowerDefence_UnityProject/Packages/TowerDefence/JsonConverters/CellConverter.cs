2027c362c8c2c41981bf63f6cd39a3370105c618
Jesse Stam <jesse@jessestam.nl>
2022-12-01 08:48:34 +0100

    Added converter for cells to reduce file size

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/JsonConverters/CellConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+using TowerDefence.World.Grid.Data;
+
+namespace TowerDefence.JsonConverters
+{
+    internal sealed class CellConverter : JsonConverter<GridSettings.Cell[]>
+    {
+        public override void WriteJson(JsonWriter writer, GridSettings.Cell[] value, JsonSerializer serializer)
+        {
+            byte[] bytes = new byte[value.Length];
+            for (int i = 0; i < value.Length; i++)
+            {
+                bytes[i] = value[i].weight;
+            }
+
+            writer.WriteValue(Convert.ToBase64String(bytes));
+        }
+
+        public override GridSettings.Cell[] ReadJson(JsonReader reader, Type objectType, GridSettings.Cell[] existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is null) return Array.Empty<GridSettings.Cell>();
+
+            var value = reader.Value as string ?? string.Empty;
+            byte[] bytes = Convert.FromBase64String(value);
+
+            var cells = new GridSettings.Cell[bytes.Length];
+            for (int i = 0; i < cells.Length; i++)
+            {
+                cells[i] = new GridSettings.Cell(bytes[i]);
+            }
+
+            return cells;
+        }
+    }
+}
