46ec726330be7522fe527f56c7ed639439769f9b
Jesse Stam <jesse@jessestam.nl>
2022-12-05 22:02:07 +0100

    Added SupportsTower variable to serializer

--- TowerDefence_UnityProject/Packages/TowerDefence/JsonConverters/CellConverter.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/JsonConverters/CellConverter.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
+using NoUtil.Debugging;
 using TowerDefence.World.Grid.Data;
+using UnityEngine;
 
 namespace TowerDefence.JsonConverters
 {
@@ -8,13 +12,16 @@ namespace TowerDefence.JsonConverters
     {
         public override void WriteJson(JsonWriter writer, GridSettings.Cell[] value, JsonSerializer serializer)
         {
-            byte[] bytes = new byte[value.Length];
+            byte[] weights = new byte[value.Length];
+            StringBuilder supportsTower = new();
             for (int i = 0; i < value.Length; i++)
             {
-                bytes[i] = value[i].weight;
+                var cell = value[i];
+                weights[i] = cell.weight;
+                supportsTower.Append(cell.supportsTower ? '1' : '0');
             }
 
-            writer.WriteValue(Convert.ToBase64String(bytes));
+            writer.WriteValue($"{Convert.ToBase64String(weights)}|{supportsTower}");
         }
 
         public override GridSettings.Cell[] ReadJson(JsonReader reader, Type objectType, GridSettings.Cell[] existingValue, bool hasExistingValue, JsonSerializer serializer)
@@ -22,12 +29,21 @@ namespace TowerDefence.JsonConverters
             if (reader.Value is null) return Array.Empty<GridSettings.Cell>();
 
             var value = reader.Value as string ?? string.Empty;
-            byte[] bytes = Convert.FromBase64String(value);
+            var split = value.Split("|");
 
-            var cells = new GridSettings.Cell[bytes.Length];
+            if (split.Length != 2)
+            {
+                $"Length does not equal 2 Length was {split.Length}".QuickCLog("Cell Converter", LogType.Error);
+                return Array.Empty<GridSettings.Cell>();
+            }
+
+            byte[] weights = Convert.FromBase64String(split[0]);
+            bool[] supportsTower = split[1].Select(x => x == '1').ToArray();
+
+            var cells = new GridSettings.Cell[weights.Length];
             for (int i = 0; i < cells.Length; i++)
             {
-                cells[i] = new GridSettings.Cell(bytes[i]);
+                cells[i] = new GridSettings.Cell(weights[i], supportsTower[i]);
             }
 
             return cells;
