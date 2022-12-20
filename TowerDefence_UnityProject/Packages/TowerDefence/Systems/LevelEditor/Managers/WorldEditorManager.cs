19e1c03721a284726d9042f20d8c08a1b494d328
Jesse Stam <jesse@jessestam.nl>
2022-12-20 18:05:05 +0100

    Added the world editor manager

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/LevelEditor/Managers/WorldEditorManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DataBinding;
+using TowerDefence.Systems.LevelEditor.Models;
+using TowerDefence.World.Grid;
+
+namespace TowerDefence.Systems.LevelEditor.Managers
+{
+    public class WorldEditorManager : IDisposable
+    {
+        private readonly ILevelEditorModel levelEditorModel;
+        private readonly BindingContext levelEditorBindingContext = new();
+        private readonly GridWorld gridWorld;
+
+        private BindingContext gridBindingContext;
+
+        public WorldEditorManager(ILevelEditorModel levelEditorModel)
+        {
+            this.levelEditorModel = levelEditorModel;
+            levelEditorBindingContext.Bind(levelEditorModel, x => x.World, OnWorldChanged);
+        }
+
+        private void OnWorldChanged(IWorldLayoutModel world)
+        {
+            gridBindingContext?.Dispose();
+            gridBindingContext = new BindingContext();
+            if (world is null) return;
+
+            gridBindingContext.Bind(world, x => x.Height, OnwWorldSizeChanged);
+            gridBindingContext.Bind(world, x => x.Width, OnwWorldSizeChanged);
+        }
+
+        private void OnwWorldSizeChanged(int _)
+        {
+            if (levelEditorModel.World is null) return;
+            var world = levelEditorModel.World;
+            int size = world.Height * world.Width;
+
+
+            while (world.Cells.Count < size)
+            {
+                world.Cells.Add(ModelFactory.Create<ICellModel>());
+            }
+
+            while (world.Cells.Count > size && world.Cells.Count != 0)
+            {
+                world.Cells.Remove(world.Cells[^1]);
+            }
+
+            if (levelEditorModel.RebuildingWorld) return;
+            levelEditorModel.RebuildingWorld = true;
+            gridWorld.CreateWorld(world.ToGridSettings())
+                .ContinueWith(() => levelEditorModel.RebuildingWorld = false)
+                .Preserve()
+                .Forget();
+        }
+
+        public void Dispose()
+        {
+            levelEditorBindingContext?.Dispose();
+            gridBindingContext?.Dispose();
+        }
+    }
+}
