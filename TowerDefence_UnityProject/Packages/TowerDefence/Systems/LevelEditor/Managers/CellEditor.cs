a9e91d3d074d1e63598e6563b0b6820d8272fc91
jesse stam <nolimet@stamdata.nl>
2023-03-06 22:28:40 +0100

    Fixed cell Editor not working

--- TowerDefence_UnityProject/Packages/TowerDefence/Systems/LevelEditor/Managers/CellEditor.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/LevelEditor/Managers/CellEditor.cs
@@ -11,15 +11,24 @@ namespace TowerDefence.Systems.LevelEditor.Managers
 {
     public class CellEditor : IDisposable
     {
-        private readonly ISelectionModel _selectionModel;
-        private readonly IWorldLayoutModel _worldLayoutModel;
-        private readonly BindingContext _bindingContext = new();
+        private readonly ISelectionModel selectionModel;
+        private readonly ILevelEditorModel levelEditorModel;
+        private IWorldLayoutModel worldLayoutModel;
 
-        public CellEditor(ISelectionModel selectionModel, IWorldLayoutModel worldLayoutModel)
+        private readonly BindingContext bindingContext = new();
+
+        public CellEditor(ISelectionModel selectionModel, ILevelEditorModel levelEditorModel)
         {
-            _selectionModel = selectionModel;
-            _worldLayoutModel = worldLayoutModel;
-            _bindingContext.Bind(_selectionModel, x => x.Selection, OnSelectionChanged);
+            this.selectionModel = selectionModel;
+            this.levelEditorModel = levelEditorModel;
+
+            bindingContext.Bind(this.selectionModel, x => x.Selection, OnSelectionChanged);
+            bindingContext.Bind(this.levelEditorModel, x => x.World, OnWorldModelChanged);
+        }
+
+        private void OnWorldModelChanged(IWorldLayoutModel world)
+        {
+            worldLayoutModel = world;
         }
 
         private void OnSelectionChanged(IList<ISelectable> obj)
@@ -29,20 +38,20 @@ namespace TowerDefence.Systems.LevelEditor.Managers
 
         public void Dispose()
         {
-            _bindingContext?.Dispose();
+            bindingContext?.Dispose();
         }
 
         public void ChangeCellsForSelection(byte weight, bool supportTower)
         {
-            foreach (var selectable in _selectionModel.Selection)
+            foreach (var selectable in selectionModel.Selection)
             {
                 switch (selectable)
                 {
                     case SelectableCell cell:
                     {
                         var cellPos = cell.GridCell.Position;
-                        var cellIndex = (int)(cellPos.x * _worldLayoutModel.Width + cellPos.y);
-                        var cellModel = _worldLayoutModel.Cells[cellIndex];
+                        var cellIndex = (int)(cellPos.x * worldLayoutModel.Width + cellPos.y);
+                        var cellModel = worldLayoutModel.Cells[cellIndex];
                         cellModel.Weight = weight;
                         cellModel.SupportsTower = supportTower;
                         break;
