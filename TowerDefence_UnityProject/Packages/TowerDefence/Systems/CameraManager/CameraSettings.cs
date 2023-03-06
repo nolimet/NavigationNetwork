50a9d02ee0ce950d64e565cee71d3f9c6ebf03f1
jesse stam <nolimet@stamdata.nl>
2023-03-06 22:40:45 +0100

    updated script to set pan speed to 1

--- TowerDefence_UnityProject/Packages/TowerDefence/Systems/CameraManager/CameraSettings.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Systems/CameraManager/CameraSettings.cs
@@ -5,7 +5,7 @@ namespace TowerDefence.Systems.CameraManager
     [Serializable]
     public class CameraSettings
     {
-        public float PanRation = 2.5f;
+        public float PanSpeed = 1f;
         public float MoveSpeed = 1f;
     }
 }
