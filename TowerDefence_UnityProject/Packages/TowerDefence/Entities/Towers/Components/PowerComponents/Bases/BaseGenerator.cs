a331336da2e6189ee5ba5b8db36b22ae03ef85a2
jesse stam <nolimet@stamdata.nl>
2023-04-10 22:43:13 +0200

    Updated BaseGenerator.cs

--- TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/PowerComponents/Bases/BaseGenerator.cs
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/PowerComponents/Bases/BaseGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Newtonsoft.Json;
-using TowerDefence.Entities.Towers.Components.BaseComponents;
 using TowerDefence.Entities.Towers.Components.Interfaces;
 using TowerDefence.Entities.Towers.Models;
 using TowerDefence.Utility;
@@ -9,27 +8,51 @@ namespace TowerDefence.Entities.Towers.Components.PowerComponents.Bases
 {
     internal abstract class BaseGenerator : IPowerProducer, IInitializable
     {
-        [JsonProperty] public double GenerationPerSecond { get; }
-        [JsonProperty] public double GenerationDelayInMs { get; } = -1;
-        [JsonProperty] public double MaxPowerBuffer { get; }
-        public double PowerBuffer { get; protected set; }
-        protected TowerSettings towerSettings { get; private set; }
+        public double GenerationPerSecond => generationPerSecond;
+        [JsonProperty] private readonly double generationPerSecond;
 
+        public double GenerationDelayInMs => generationDelayInMs;
+        [JsonProperty] private readonly double generationDelayInMs = -1;
+
+        public double MaxPowerBuffer => maxPowerBuffer;
+        [JsonProperty] private readonly double maxPowerBuffer;
+
+        protected IPowerTargetFinder PowerTargetFinder { get; private set; }
+        public double PowerBuffer { get; protected set; }
         protected double delayTimer;
 
         public virtual void PostInit(ITowerObject towerObject, ITowerModel model)
         {
-            towerSettings = model.Components.GetComponent<TowerSettings>();
+            PowerTargetFinder = model.Components.GetComponent<IPowerTargetFinder>();
         }
 
-        public virtual void PowerTick(float delta)
+        public virtual void PowerTick(double delta)
         {
             if (delayTimer <= 0)
             {
-                double generationMult = GenerationDelayInMs > 1 ? GenerationDelayInMs / 1000 : 1;
-                double addedAmount = GenerationPerSecond * generationMult * delta;
+                var generationMult = GenerationDelayInMs > 1 ? GenerationDelayInMs / 1000 : 1;
+                var addedAmount = GenerationPerSecond * generationMult * delta;
                 PowerBuffer = Math.Min(MaxPowerBuffer, PowerBuffer + addedAmount);
                 delayTimer = GenerationDelayInMs / 1000;
+
+                var maxPowerPush = PowerBuffer / PowerTargetFinder.Targets.Count;
+                var length = PowerTargetFinder.Targets.Count;
+                for (var i = 0; i < length; i++)
+                    switch (PowerTargetFinder.Targets[i])
+                    {
+                        case IPowerConsumer consumer:
+                        {
+                            var accepted = consumer.PushPower(maxPowerPush);
+                            maxPowerPush += (accepted - maxPowerPush) / (length - i);
+                            break;
+                        }
+                        case IPowerBuffer buffer:
+                        {
+                            var accepted = buffer.PushPower(maxPowerPush);
+                            maxPowerPush += (accepted - maxPowerPush) / (length - i);
+                            break;
+                        }
+                    }
             }
 
             delayTimer -= delta;
@@ -37,7 +60,6 @@ namespace TowerDefence.Entities.Towers.Components.PowerComponents.Bases
 
         public virtual void Dispose()
         {
-            
         }
     }
 }
