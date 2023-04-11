ab374d5e32b5100fb29200e3808650022e4a7ce0
Jesse Stam <jesse@jessestam.nl>
2023-04-11 08:42:43 +0200

    Added void tower

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/PowerComponents/VoidPowerConsumer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using TowerDefence.Entities.Components;
+using TowerDefence.Entities.Towers.Components.Interfaces;
+
+namespace TowerDefence.Entities.Towers.Components.PowerComponents
+{
+    [Serializable]
+    [Component(ComponentType.Tower, typeof(IPowerComponent), AllowMultiple = false)]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class VoidPowerConsumer : IPowerConsumer
+    {
+        public double MaxStored => 999999999;
+        public double Stored => 0;
+
+        public bool TryConsume(double amount)
+        {
+            return true;
+        }
+
+        public double PushPower(double maxAmount)
+        {
+            return maxAmount;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
