4a6d93893b007aec61a706ef5e58e93f56dc5d6d
Jesse Stam <nolimet@stamdata.nl>
2023-05-05 21:58:05 +0200

    added soem stuff to shader

--- TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
+++ TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
@@ -15,6 +15,8 @@ void get_tile_data_half(const float2 uv, const UnityTexture2D tile_map, const fl
 }
 
 void draw_tile(const float4 supports_tower_color, const float4 supports_no_tower_color, const float2 chunk_count, UnityTexture2D mask, UnityTexture2D tile_background, UnityTexture2D tile_forground)
-
+{
+    
+}
 
 #endif
