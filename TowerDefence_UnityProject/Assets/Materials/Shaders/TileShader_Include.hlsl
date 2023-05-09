e6b0f47eaa3bb2543d4cc657d4c831c572f218ac
Jesse Stam <jesse@jessestam.nl>
2023-05-09 18:56:12 +0200

    Worked on the shader include

--- TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
+++ TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
@@ -7,16 +7,16 @@ float4 sample_texture(const UnityTexture2D tex, float uv)
     return SAMPLE_TEXTURE2D(tex, tex.samplerstate, uv);
 }
 
-void get_tile_data_half(const float2 uv, const UnityTexture2D tile_map, const float2 chunk_size, out float towerHeight, out bool supportsTower)
+void get_tile_data_half(const float2 uv, const UnityTexture2D tile_map, out half towerHeight,out bool supportsTower)
 {
     half2 pixel = sample_texture(tile_map, uv);
     towerHeight = pixel.r;
     supportsTower = pixel.g > 0.5;
 }
 
-void draw_tile(const float4 supports_tower_color, const float4 supports_no_tower_color, const float2 chunk_count, UnityTexture2D mask, UnityTexture2D tile_background, UnityTexture2D tile_forground)
+void draw_tile_half(const float4 supports_tower_color, const float4 supports_no_tower_color, const float2 chunk_count,
+                    const UnityTexture2D mask, const UnityTexture2D tile_background, const UnityTexture2D tile_forground)
 {
-    
 }
 
 #endif
