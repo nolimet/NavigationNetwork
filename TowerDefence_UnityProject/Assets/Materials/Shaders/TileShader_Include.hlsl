c97911c4e9572a47e95f9dbedec19f199965292a
Jesse Stam <jesse@jessestam.nl>
2023-05-10 08:30:59 +0200

    lowered shader precision s to lower memory usage

--- TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
+++ TowerDefence_UnityProject/Assets/Materials/Shaders/TileShader_Include.hlsl
@@ -7,18 +7,18 @@ float4 sample_texture(const UnityTexture2D tex, float2 uv)
     return SAMPLE_TEXTURE2D(tex, tex.samplerstate, uv);
 }
 
-void get_tile_data_half(const float2 uv, const UnityTexture2D tile_map, out half towerHeight, out bool supportsTower)
+void get_tile_data_half(const half2 uv, const UnityTexture2D tile_map, out float tower_height, out bool supports_tower)
 {
-    half2 pixel = sample_texture(tile_map, uv);
-    towerHeight = pixel.r;
-    supportsTower = pixel.g > 0.5;
+    half4 pixel = sample_texture(tile_map, uv);
+    tower_height = pixel.r;
+    supports_tower = pixel.g > 0.5;
 }
 
-void draw_tile_float(const float2 uv, half tower_height, bool supports_tower, const float4 supports_tower_color, const float4 supports_no_tower_color, const float4 background_tile_color, const float2 group_size,
-                     const UnityTexture2D mask, const UnityTexture2D tile_background, const UnityTexture2D tile_foreground, out float4 color)
+void draw_tile_half(const half2 uv, const half tower_height, const bool supports_tower, const half4 supports_tower_color, const half4 supports_no_tower_color, const half4 background_tile_color, const half2 group_size,
+                    const UnityTexture2D mask, const UnityTexture2D tile_background, const UnityTexture2D tile_foreground, out half4 color)
 {
-    const float2 repeat_uv = uv * group_size % 1;
-    float mask_value = sample_texture(mask, repeat_uv);
+    const half2 repeat_uv = uv * group_size % 1;
+    half mask_value = sample_texture(mask, repeat_uv);
 
     if (mask_value < .5)
     {
@@ -26,16 +26,15 @@ void draw_tile_float(const float2 uv, half tower_height, bool supports_tower, co
     }
     else
     {
-        float4 foreground_color = sample_texture(tile_foreground, repeat_uv);
+        half4 foreground_color = sample_texture(tile_foreground, repeat_uv);
         if (supports_tower)
         {
-            foreground_color *= supports_tower_color;
+            color = foreground_color * supports_tower_color;
         }
         else
         {
-            foreground_color *= supports_no_tower_color;
+            color = foreground_color * supports_no_tower_color;
         }
-        color = foreground_color;
     }
 }
 
