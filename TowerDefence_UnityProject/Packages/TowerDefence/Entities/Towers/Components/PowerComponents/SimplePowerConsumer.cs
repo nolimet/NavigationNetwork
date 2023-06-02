04d6c20b4df283daf7cc1f987d2bdadb82d0aa29
Jesse Stam <jesse@jessestam.nl>
2023-06-02 08:49:41 +0200

    Added SimplePowerConsumer

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/PowerComponents/SimplePowerConsumer.cs
@@ -0,0 +1,33 @@
+﻿using TowerDefence.Entities.Towers.Components.Interfaces;
+
+namespace TowerDefence.Entities.Towers.Components.PowerComponents
+{
+    public class SimplePowerConsumer : IPowerConsumer
+    {
+        public double MaxStored { get; private set; } = 1000;
+        public double Stored { get; protected set; }
+
+        public bool TryConsume(double amount)
+        {
+            if (Stored < amount) return false;
+            Stored -= amount;
+            return true;
+        }
+
+        public double PushPower(double maxAmount)
+        {
+            if (Stored + maxAmount > MaxStored)
+            {
+                double added = MaxStored - Stored;
+                Stored = MaxStored;
+                return added;
+            }
+
+            return maxAmount;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
