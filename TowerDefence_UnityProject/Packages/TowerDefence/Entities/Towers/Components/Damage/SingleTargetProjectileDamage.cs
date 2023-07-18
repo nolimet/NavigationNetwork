cd47ef703a9119c355c6283267406e6cb0d7cca0
Jesse Stam <jesse@jessestam.nl>
2023-07-18 08:39:30 +0200

    Added SingleTargetProjectDamageComponent

---
+++ TowerDefence_UnityProject/Packages/TowerDefence/Entities/Towers/Components/Damage/SingleTargetProjectileDamage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using TowerDefence.Entities.Components;
+using TowerDefence.Entities.Enemies;
+using TowerDefence.Entities.Towers.Components.Interfaces;
+using TowerDefence.Entities.Towers.Models;
+
+namespace TowerDefence.Entities.Towers.Components.Damage
+{
+    [Component(ComponentType.Tower, typeof(IDamageComponent))]
+    public class SingleTargetProjectileDamage : IDamageComponent, IInitializable
+    {
+        public event Action<IEnumerable<IEnemyObject>> AppliedDamageToTargets;
+        public double DamagePerSecond { get; private set; }
+        public void Tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostInit(ITowerObject towerObject, ITowerModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
